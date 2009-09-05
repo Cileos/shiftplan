fe68abd2ed0950e7e91f9d58125c26b2721dee06
Clemens Kofler <clemens@railway.at>
2009-09-05 18:35:48 +0200

    First stabs at integrating qualifications

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,8 @@ class Employee < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name
 
+  acts_as_taggable_on :qualifications
+
   def full_name
     "#{first_name} #{last_name}"
   end
