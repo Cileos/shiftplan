df87a41cabc49600fb983a8c92bc8f6a07a9b71d
Clemens Kofler <clemens@railway.at>
2009-09-20 19:56:43 +0200

    Tiny.

--- public/javascripts/shiftplan/tabs.js
+++ public/javascripts/shiftplan/tabs.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
   var selected_tab = 0;
   var hash = document.location.hash;
 
-  if(hash.search(/^#workplaces$/i) > -1) {
+  if(hash.search(/^#plans$/i) > - 1) {
     selected_tab = 1;
-  } else if(hash.search(/^#employees$/i) > -1) {
+  } else if(hash.search(/^#workplaces$/i) > -1) {
     selected_tab = 2;
-  } else if(hash.search(/^#plans$/i) > - 1) {
+  } else if(hash.search(/^#employees$/i) > -1) {
     selected_tab = 3;
   }
 
