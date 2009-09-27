3daebb5874145ee11f103a98142bec81059f6330
Clemens Kofler <clemens@railway.at>
2009-09-27 18:02:15 +0200

    highlight suitable workplaces when starting to drag an employee

--- public/javascripts/shiftplan/plan/employee.js
+++ public/javascripts/shiftplan/plan/employee.js
@@ -6,6 +6,14 @@ $.extend(Employee, Resource);
 
 $.extend(Employee, {
 	selector: ".employee",
+	on_drag_start: function(event, ui) {
+	  // TODO: should also take into account that the employee might already be assigned to this or another shift
+	  var possible_workplaces = ui.helper.closest('.employee').attr('data-possible-workplaces').split(', ');
+	  $('.shift:not(.' + possible_workplaces.join(', .') + ')').addClass('unsuitable_workplace');
+	},
+	on_drag_stop: function(event, ui) {
+	  $('.unsuitable_workplace').removeClass('unsuitable_workplace');
+	}
 });
 
 Employee.prototype = $.extend(new Resource, {
@@ -14,7 +22,9 @@ Employee.prototype = $.extend(new Resource, {
   },
 	bind_events: function() {
 		$('div', this.element).draggable({
-			helper: 'clone'
+			helper: 'clone',
+			start: Employee.on_drag_start,
+			stop: Employee.on_drag_stop
 		});
 	}
 });
