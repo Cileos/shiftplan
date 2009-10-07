6dffa21c42a7d3e464ee71fdae75c0d16563d22d
Clemens Kofler and Sven Fuchs <dev+clemens+sven@adva-cms.org>
2009-10-08 00:26:24 +0200

    Removed mirror 'vendor/plugins/steam'

--- vendor/plugins/steam/lib/core_ext/ruby/string/underscore.rb
+++
@@ -1,5 +0,0 @@
-class String
-  def underscore
-    self[0, 1].downcase + self[1..-1].gsub(/[A-Z]/) { |c| "_#{c.downcase}" }
-  end
-end
--- vendor/plugins/steam/lib/steam/browser.rb
+++
@@ -1,6 +0,0 @@
-module Steam
-  module Browser
-    autoload :HtmlUnit,   'steam/browser/html_unit'
-    autoload :Connection, 'steam/browser/html_unit/connection'
-  end
-end
