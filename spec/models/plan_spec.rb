74092af07066d4ed899a4acde95623f5e3ead821
Clemens Kofler <clemens@railway.at>
2009-10-11 14:31:32 +0200

    Align Plan specs with other specs.

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -2,27 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Plan do
   def monday_morning
-    @monday ||= Time.parse('2009-09-07 08:30')
+    @monday ||= Time.local(2009, 9, 7, 8, 30)
   end
 
   def friday_afternoon
-    @friday ||= Time.parse('2009-09-11 16:30')
+    @friday ||= Time.local(2009, 9, 11, 16, 30)
   end
 
   before(:each) do
-    @plan = Plan.new do |p|
-      p.account_id = 1
-      p.start      = monday_morning
-      p.end        = friday_afternoon
-      p.duration   = 8 * 60
-
-      p.shifts = [Shift.new(:start => monday_morning, :end => friday_afternoon)]
-    end
+    @plan = Plan.new
   end
 
   describe "associations" do
-    it "has many shifts" do
-      @plan.shifts.should_not be_empty
+    it "should have shifts" do
+      @plan.should have_many(:shifts)
     end
   end
 
@@ -31,22 +24,52 @@ describe Plan do
   end
 
   describe "callbacks" do
-    it "should set the duration from start/end time before validation" do
-      @plan.duration = nil
-      @plan.valid?
-      @plan.duration.should == 8 * 60
+    describe "#set_duration" do
+      before(:each) do
+        @plan.start = monday_morning
+        @plan.end   = friday_afternoon
+      end
+
+      it "should set the duration from start/end time before validation" do
+        @plan.duration = nil
+        @plan.valid? # @plan.send(:set_duration) # ?
+        @plan.duration.should == 8 * 60
+      end
     end
   end
 
-  it "days returns a range of days from start_day to end_day" do
-    @plan.days.should == (Date.parse('2009-09-07')..Date.parse('2009-09-11'))
-  end
+  describe "instance methods" do
+    describe "#days" do
+      before(:each) do
+        @plan.start = monday_morning
+        @plan.end   = friday_afternoon
+      end
 
-  it "start_time_in_minutes returns the start_time in minutes" do
-    @plan.start_time_in_minutes.should == 8 * 60 + 30
-  end
+      it "should return a range of days from start day to end day" do
+        @plan.days.should == (Date.civil(2009, 9, 7)..Date.civil(2009, 9, 11))
+      end
+    end
 
-  it "end_time_in_minutes returns the end_time in minutes" do
-    @plan.end_time_in_minutes.should == 16 * 60 + 30
+    describe "#start_time_in_minutes" do
+      before(:each) do
+        @plan.start = monday_morning
+        @plan.end   = friday_afternoon
+      end
+
+      it "should return the start time in minutes" do
+        @plan.start_time_in_minutes.should == 8 * 60 + 30
+      end
+    end
+
+    describe "#end_time_in_minutes" do
+      before(:each) do
+        @plan.start = monday_morning
+        @plan.end   = friday_afternoon
+      end
+
+      it "should return the end time in minutes" do
+        @plan.end_time_in_minutes.should == 16 * 60 + 30
+      end
+    end
   end
 end
