77801e5db0067633eed8c8622e4058c508be57fa
Fritz Thielemann <fritz.thielemann@gmail.com>
2009-11-09 17:23:20 +0100

    taking out env var setting because no local test runs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,3 @@ config.action_mailer.delivery_method = :test
 # config.gem 'langalex-culerity', :source => 'http://gems.github.com'
 
 config.i18n.default_locale = :en
-
-ENV['JAVA_HOME'] = "/usr/lib/jvm/java-1.5.0-sun"
-ENV['LD_LIBRARY_PATH'] = "#{ENV['LD_LIBRARY_PATH']}:#{ENV['JAVA_HOME]}/jre/lib/i386:#{ENV['JAVA_HOME']}/jre/lib/i386/client"
