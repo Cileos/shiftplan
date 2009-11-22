d2868469a45a9bc2653176bd4166122a20c10f38
Clemens Kofler <clemens@railway.at>
2009-11-22 18:46:34 +0100

    Implement Availability.for(day) / Availability.for(start, end). Both use overridden availabilities and fall back to defaults if no overrides can be found.

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -5,4 +5,25 @@ class Availability < ActiveRecord::Base
 
   named_scope :default,  :conditions => "day IS NULL AND day_of_week IS NOT NULL"
   named_scope :override, :conditions => "day_of_week IS NULL AND day IS NOT NULL"
+
+  class << self
+    def for(*args)
+      start_date, end_date = *args
+      end_date   ||= start_date
+      date_range   = start_date..end_date
+      days_of_week = date_range.map(&:wday).uniq
+
+      availabilities = all(:conditions => ["day BETWEEN ? AND ? OR day_of_week IN(?)", start_date, end_date, days_of_week])
+
+      availabilities = date_range.inject(ActiveSupport::OrderedHash.new) do |by_day, day|
+        by_day.merge(day => begin
+          for_day = availabilities.select { |a| a.day == day }
+          # select the defaults for the given day if no availabilities are defined
+          for_day.empty? ? availabilities.select { |a| a.day_of_week = day.wday } : for_day
+        end)
+      end
+
+      args.size == 1 ? availabilities[start_date] : availabilities
+    end
+  end
 end
--- spec/models/availability_spec.rb
+++ spec/models/availability_spec.rb
@@ -40,31 +40,94 @@ describe Availability do
   describe "scopes" do
     describe ".default" do
       before(:each) do
-        @default_availability = Availability.create!(:employee_id => 1, :day_of_week => 0,    :start => '09:00', :end => '17:00')
-        @override             = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '09:00', :end => '17:00')
+        @sunday_default  = Availability.create!(:employee_id => 1, :day_of_week => 0,    :start => '09:00', :end => '17:00')
+        @sunday_override = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '08:00', :end => '16:00')
       end
 
-      it "should include default availabilites" do
-        Availability.default.should include(@default_availability)
+      it "should include default availabilities" do
+        Availability.default.should include(@sunday_default)
       end
 
       it "should not include overrides" do
-        Availability.default.should_not include(@override)
+        Availability.default.should_not include(@sunday_override)
       end
     end
 
     describe ".override" do
       before(:each) do
-        @default_availability = Availability.create!(:employee_id => 1, :day_of_week => 0,    :start => '09:00', :end => '17:00')
-        @override             = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '09:00', :end => '17:00')
+        @sunday_default  = Availability.create!(:employee_id => 1, :day_of_week => 0,    :start => '09:00', :end => '17:00')
+        @sunday_override = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '08:00', :end => '16:00')
       end
 
       it "should include overrides" do
-        Availability.override.should include(@override)
+        Availability.override.should include(@sunday_override)
       end
 
-      it "should not include default availabilites" do
-        Availability.override.should_not include(@default_availability)
+      it "should not include default availabilities" do
+        Availability.override.should_not include(@sunday_default)
+      end
+    end
+  end
+
+  describe "class methods" do
+    describe ".for" do
+      before(:each) do
+        @sunday_default    = Availability.create!(:employee_id => 1, :day_of_week => 0,    :start => '09:00', :end => '17:00')
+        @monday_default    = Availability.create!(:employee_id => 1, :day_of_week => 1,    :start => '06:00', :end => '14:00')
+        @monday_default_2  = Availability.create!(:employee_id => 1, :day_of_week => 1,    :start => '16:00', :end => '23:00')
+        @sunday_override   = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '06:00', :end => '14:00')
+        @sunday_override_2 = Availability.create!(:employee_id => 1, :day => '2009-11-22', :start => '16:00', :end => '23:00')
+      end
+
+      describe "with only one parameter (= day)" do
+        it "should include the overrides if at least one override is defined for the given day" do
+          availabilities = Availability.for(Date.civil(2009, 11, 22))
+          availabilities.should include(@sunday_override)
+          availabilities.should include(@sunday_override_2)
+        end
+
+        it "should not include the defaults if at least one override is defined for the given day" do
+          Availability.for(Date.civil(2009, 11, 22)).should_not include(@sunday_default)
+        end
+
+        it "should include the defaults if no override is defined for the given day" do
+          availabilities = Availability.for(Date.civil(2009, 11, 23))
+          availabilities.should include(@monday_default)
+          availabilities.should include(@monday_default_2)
+        end
+
+        it "should be empty if no overrides and no defaults are defined for the given day" do
+          Availability.for(Date.civil(2009, 11, 24)).should be_empty
+        end
+      end
+
+      describe "with two parameters (= start and end day)" do
+        it "should include the overrides for the given days and fall back to defaults if necessary" do
+          start_date = Date.civil(2009, 11, 22)
+          end_date   = Date.civil(2009, 11, 23)
+
+          availabilities = Availability.for(start_date, end_date)
+          availabilities[start_date].should include(@sunday_override)
+          availabilities[start_date].should include(@sunday_override_2)
+          availabilities[end_date].should   include(@monday_default)
+          availabilities[end_date].should   include(@monday_default_2)
+        end
+
+        it "should not include the defaults for the given days if overrides are defined" do
+          start_date = Date.civil(2009, 11, 22)
+          end_date   = Date.civil(2009, 11, 23)
+
+          availabilities = Availability.for(start_date, end_date)
+          availabilities[start_date].should_not include(@sunday_default)
+        end
+
+        it "should be empty if no overrides and no defaults are defined for the given days" do
+          start_date = Date.civil(2009, 11, 24)
+          end_date   = Date.civil(2009, 11, 26)
+
+          availabilities = Availability.for(start_date, end_date)
+          (start_date..end_date).each { |day| availabilities[day].should be_empty }
+        end
       end
     end
   end
