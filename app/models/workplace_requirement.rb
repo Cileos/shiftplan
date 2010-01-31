155c8e8496643fec82cd0a7045d597ed0dbfcea3
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-31 13:23:11 +0100

    comment out Workplace validates_presence_of :workplace_id because new workplace form ajax submit failed when default staffing given

--- app/models/workplace_requirement.rb
+++ app/models/workplace_requirement.rb
@@ -3,6 +3,7 @@ class WorkplaceRequirement < ActiveRecord::Base
   belongs_to :workplace
   belongs_to :qualification
 
-  validates_presence_of :workplace_id,     :if => lambda { |record| record.workplace.nil? }
+  # FIXME causes new workplace form submit to fail if default staffing given
+  # validates_presence_of :workplace_id,     :if => lambda { |record| record.workplace.nil? }
   validates_presence_of :qualification_id, :if => lambda { |record| record.qualification.nil? }
 end
