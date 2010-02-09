8a21ba7c8ad5e89f5fc8a73951152c6f8d2d386e
Sven Fuchs <svenfuchs@artweb-design.de>
2010-02-10 00:50:12 +0100

    upstream change from steam

--- features/step_definitions/shift_steps.rb
+++ features/step_definitions/shift_steps.rb
@@ -101,5 +101,5 @@ Then /^the shift "([^\"]*)" on (.*) should not be marked unsuitable$/ do |workpl
 end
 
 Then /^no shifts should be marked as unsuitable$/ do
-  response.body.join.should_not match(/unsuitable_workplace/)
+  response.body.should_not match(/unsuitable_workplace/)
 end
