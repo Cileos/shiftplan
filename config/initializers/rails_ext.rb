9190dca60f8678d2c54e1f3e927f638e98a88281
Clemens Kofler <clemens@railway.at>
2010-02-11 09:55:46 +0100

    fix issue with i18n and html_safe

---
+++ config/initializers/rails_ext.rb
@@ -0,0 +1,6 @@
+# ugh - see https://rails.lighthouseapp.com/projects/8994/tickets/3800-arrays-from-i18n-files-and-html_safe
+class Array
+  def html_safe
+    each {|e| e.html_safe unless e.nil? }
+  end
+end
