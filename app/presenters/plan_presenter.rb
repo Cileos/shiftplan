a18e0e8e48be283bd1bd3c38a9c28c974cdaa495
Clemens Kofler <clemens@railway.at>
2010-02-13 16:01:01 +0100

    make presenters html_safe

--- app/presenters/plan_presenter.rb
+++ app/presenters/plan_presenter.rb
@@ -11,7 +11,7 @@ class PlanPresenter < Presenter
   def render
     div(attributes) do
       h1(plan.name + span(plan_dates(plan), :class => 'dates')) +
-      days.map { |day| presenter_for(day, 'plan/day').render(Array(shifts.by_day[day])) }.join
+      days.map { |day| presenter_for(day, 'plan/day').render(Array(shifts.by_day[day])) }.join.html_safe
     end
   end
 end
