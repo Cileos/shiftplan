6cf77a15fc046ab0153688b2fe0a69bf228c5611
Clemens Kofler <clemens@railway.at>
2010-02-21 21:16:30 +0100

    cut down unnecessary comments

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -1,24 +1,8 @@
 Shiftplan::Application.configure do
-  config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
-  # Show full error reports and disable caching
+  config.cache_classes                                 = false
+  config.whiny_nils                                    = true
   config.action_controller.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
-
-  # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-
-  # config.gem 'cucumber',    :lib => false,        :version => '>=0.3.103' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
-  # config.gem 'rspec',       :lib => false,        :version => '>=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
-  # config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
-
-  # config.gem 'webrat',      :lib => false,        :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
+  config.action_mailer.delivery_method                 = :test
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,4 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_shiftplan_session',
   :secret      => 'd85737ffefa5b429be2d8ee838b853621a8bfd724f0b7a0278517602eae830de8b1149397df5d50a84142f4df41e93e106f44fc30f5b67fec70c1b678a0baf32'
 }
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
