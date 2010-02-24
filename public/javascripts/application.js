11e155b310a9f7dcc14c306a6d4276fabba0fde3
Clemens Kofler <clemens@railway.at>
2010-02-24 20:57:40 +0100

    improve AJAX handling so it only tries to access the html hash if it's actually present

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -35,19 +35,21 @@ $(document).ready(function() {
       $('#staff_requirements').each(function() { $(this).empty(); })
       $('#copy_from_options').hide();
 
-      if(data['html']['append']) {
-        $.each(data['html']['append'], function(element, html) {
-          // folding does not work well with table rows
-          // $(html).css('display', 'none').appendTo($(element)).effect('fold', { mode: 'show' }, 1000).effect('highlight', {}, 1000);
-          $(html).appendTo($(element)).effect('highlight', {}, 1000);
-        });
-      }
+      if(data['html']) {
+        if(data['html']['append']) {
+          $.each(data['html']['append'], function(element, html) {
+            // folding does not work well with table rows
+            // $(html).css('display', 'none').appendTo($(element)).effect('fold', { mode: 'show' }, 1000).effect('highlight', {}, 1000);
+            $(html).appendTo($(element)).effect('highlight', {}, 1000);
+          });
+        }
 
-      if(data['html']['replace']) {
-        $.each(data['html']['replace'], function(element, html) {
-          // $(html).css('display', 'none').replaceAll($(element)).effect('highlight', {}, 1000);
-          $(html).replaceAll($(element)).effect('highlight', {}, 1000);
-        });
+        if(data['html']['replace']) {
+          $.each(data['html']['replace'], function(element, html) {
+            // $(html).css('display', 'none').replaceAll($(element)).effect('highlight', {}, 1000);
+            $(html).replaceAll($(element)).effect('highlight', {}, 1000);
+          });
+        }
       }
 
       if(data['flash']) {
