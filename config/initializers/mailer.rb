bf57b4c825fa55542eb5b083ee8bf0cca583e38f
Clemens Kofler <clemens@railway.at>
2010-03-28 22:03:46 +0200

    fix deprecation warning

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -11,7 +11,7 @@ ActionMailer::Base.smtp_settings = {
 #needs to be set for devise (http://github.com/plataformatec/devise/issues/closed#issue/139)
 # FIXME
 #after switching to rails HEAD we have to re-check this
-if RAILS_ENV == 'production'
+if Rails.env == 'production'
   ActionMailer::Base.default_url_options[:host] = "shiftplandemo.heroku.com"
 else
   ActionMailer::Base.default_url_options[:host] = "localhost:3000"
