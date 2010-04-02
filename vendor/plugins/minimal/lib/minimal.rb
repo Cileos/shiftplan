464d89a8937e3dfba090c8233815acea655128b6
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-02 22:03:17 +0200

    remove vendored gems

--- vendor/plugins/minimal/lib/minimal.rb
+++
@@ -1,6 +0,0 @@
-require 'action_view'
-
-module Minimal
-  autoload :Template, 'minimal/template'
-  autoload :Tidy,     'minimal/tidy'
-end
--- vendor/plugins/minimal/test/fixtures/views/foo/_partial.rb
+++
@@ -1,7 +0,0 @@
-module Foo
-  class Partial < Minimal::Template
-    def content
-      p local
-    end
-  end
-end
