a9d39d56119dcac4c0da691ef05152eb7d8b6f6d
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-03 18:05:16 +0200

    use _employee.rb instead of _employee.html.rb

---
+++ app/views/employees/_employee.rb
@@ -0,0 +1,16 @@
+module Employees
+  class Employee < Minimal::Template
+    def content
+      li_for(employee, :class => "#{employee.state} resource", :'data-form-values' => employee.form_values_json) do
+        image_tag(employee.gravatar_url(:size => 60))
+        div(:class => 'actions') { link_to_destroy(employee) }
+        h2(employee.full_name, :class => 'name')
+        p(employee.possible_workplaces.map(&:name).join(', '), :class => 'qualifications')
+      end
+    end
+    
+    def link_to_destroy(employee)
+      link_to(t(:destroy), employee_path(employee), :class => 'delete', :method => :delete, :confirm => t(:employee_delete_confirmation))
+    end
+  end
+end
