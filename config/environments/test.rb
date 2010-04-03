296358e5b88d8d2bd60db0ce0f9d4761d3f733ba
Clemens Kofler <clemens@railway.at>
2010-04-03 23:03:50 +0200

    make specs run again

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,7 +3,6 @@ Shiftplan::Application.configure do
   config.whiny_nils                                 = true
   config.consider_all_requests_local                = true
   config.action_controller.perform_caching          = false
-  config.action_view.cache_template_loading         = false
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.delivery_method              = :test
   config.colorize_logging                           = false
