3e327d3a52fe86b7e9c64c8bd5255829a01de67b
Clemens Kofler <clemens@railway.at>
2010-04-04 02:23:24 +0200

    don't seem to need to register the csv type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Mime::Type.register "text/csv", :csv
+# Mime::Type.register "text/csv", :csv
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
