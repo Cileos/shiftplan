d9f515bc5ac6867bbd16778c9ad340a313564b0c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-11 19:07:16 +0200

    move dashboard menu to the top

--- app/views/dashboards/show.rb
+++ app/views/dashboards/show.rb
@@ -1,11 +1,11 @@
 class Dashboards::Show < Minimal::Template
   def content
-    h1 t(:'dashboard.overview')
-    render :partial => 'activities/list', :locals => { :activities => current_activities }
-    render :partial => 'activities/list', :locals => { :activities => aggregated_activities }
-
     h2 t(:'dashboard.master_data')
     render :partial => 'master_data'
+
+    h1 t(:'dashboard.overview'), :style => 'clear: both;'
+    render :partial => 'activities/list', :locals => { :activities => current_activities }
+    render :partial => 'activities/list', :locals => { :activities => aggregated_activities }
 
     render :partial => 'sidebar'
   end
