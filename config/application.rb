06812cc45b2ebe7e52782dd0dfb5c588da2809ff
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-11 16:33:49 +0200

    move session store config to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module Shiftplan
     config.i18n.default_locale = :de
     config.filter_parameters += [:password, :password_confirmation]
     config.active_record.observers = [:'activity/resource_observer', :'activity/element_observer']
+
+    config.session_store :cookie_store, :key => '_shiftplan_session'
+    config.cookie_secret = 'd85737ffefa5b429be2d8ee838b853621a8bfd724f0b7a0278517602eae830de8b1149397df5d50a84142f4df41e93e106f44fc30f5b67fec70c1b678a0baf32'
   end
 end
