84657e31dd805333c50cda8f869ed34a66a58da9
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-11 22:20:54 +0200

    memoizing with arguments sux

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -12,7 +12,8 @@ class Requirement < ActiveRecord::Base
   delegate :day, :start, :end, :to => :shift
 
   def suitable_employees(statuses)
-    @suitable_employees ||= begin
+    @suitable_employees ||= {}
+    @suitable_employees[statuses] ||= begin
       Employee.for_qualification(qualification).all.select do |employee|
         employee.statuses.for(shift.day).any? do |status|
           status.start_time.strftime('%H%M%S') <= self.start.strftime('%H%M%S') &&
