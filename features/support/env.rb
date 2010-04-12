1e238962ea5515c3b70464bb3384ef299f439473
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-12 18:32:37 +0200

    notes about database_cleaner

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,3 +55,13 @@ Before do
     end
   end
 end
+
+# # How to clean your database when transactions are turned off. See
+# # http://github.com/bmabey/database_cleaner for more info.
+# if defined?(ActiveRecord::Base)
+#   begin
+#     require 'database_cleaner'
+#     DatabaseCleaner.strategy = :truncation
+#   rescue LoadError => ignore_if_database_cleaner_not_present
+#   end
+# end
