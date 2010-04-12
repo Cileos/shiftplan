994e398647c294bfa0d47ea4247c8e3384537a46
Clemens Kofler <clemens@railway.at>
2010-04-12 15:16:11 +0200

    [Fix issue #58] have correct days for default availabilities linked (based on first day of week according to locale)

--- app/views/default_statuses/_employee.rb
+++ app/views/default_statuses/_employee.rb
@@ -3,7 +3,7 @@ class DefaultStatuses::Employee < Minimal::Template
     tr do
       th(:scope => 'row') { a(employee.full_name, :name => employee_default_availabilities_dom_id(employee)) }
       td { link_to(t(:override_defaults), employee_statuses_path(employee)) }
-      0.upto(6) do |day_of_week|
+      numeric_days_of_week.each do |day_of_week|
         locals = { :employee => employee, :day_of_week => day_of_week, :statuses => Status.fill_gaps!(employee, day_of_week, default_statuses[day_of_week]) }
         render(:partial => 'day', :locals => locals)
       end
--- app/views/statuses/_default_statuses.rb
+++ app/views/statuses/_default_statuses.rb
@@ -5,7 +5,7 @@ class Statuses::DefaultStatuses < Minimal::Template
     table(:class => "#{dom_id(employee)}_defaults defaults") do
       tbody do
         tr do
-          0.upto(6) do |day_of_week|
+          numeric_days_of_week.each do |day_of_week|
             locals = { :employee => employee, :day_of_week => day_of_week, :statuses => Status.fill_gaps!(employee, day_of_week, default_statuses[day_of_week]) }
             render(:partial => 'default_statuses/day', :locals => locals)
           end
