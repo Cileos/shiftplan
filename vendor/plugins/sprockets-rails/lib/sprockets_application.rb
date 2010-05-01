fee7b9652c290d7c33b7ebc39c1fca0b61a53b58
Clemens Kofler <clemens@railway.at>
2010-05-01 14:27:39 +0200

    getting rid of some deprecation warnings

--- vendor/plugins/sprockets-rails/lib/sprockets_application.rb
+++ vendor/plugins/sprockets-rails/lib/sprockets_application.rb
@@ -21,11 +21,13 @@ module SprocketsApplication
 
     protected
       def secretary
-        @secretary ||= Sprockets::Secretary.new(configuration.merge(:root => RAILS_ROOT))
+        # @secretary ||= Sprockets::Secretary.new(configuration.merge(:root => RAILS_ROOT))
+        @secretary ||= Sprockets::Secretary.new(configuration.merge(:root => Rails.root))
       end
     
       def configuration
-        YAML.load(IO.read(File.join(RAILS_ROOT, "config", "sprockets.yml"))) || {}
+        # YAML.load(IO.read(File.join(RAILS_ROOT, "config", "sprockets.yml"))) || {}
+        YAML.load(IO.read(Rails.root.join("config/sprockets.yml"))) || {}
       end
 
       def concatenation
