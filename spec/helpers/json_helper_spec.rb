965fb53e09033b094e3ec802a22ff12750950a53
Clemens Kofler <clemens@railway.at>
2010-06-28 10:22:48 +0200

    resurrect helper specs

--- spec/helpers/json_helper_spec.rb
+++ spec/helpers/json_helper_spec.rb
@@ -1,30 +1,28 @@
-# FIXME: helper specs don't work with rspec 2 yet ...
-#
-# require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-# 
-# describe JsonHelper do
-#   describe "#object" do
-#     before(:each) do
-#       helper.instance_variable_set(:@foo, 'bar')
-#       helper.instance_variable_set(:@object_name, 'foo')
-#     end
-# 
-#     it "should return the object referenced by @object_name" do
-#       helper.object.should == 'bar'
-#     end
-#   end
-# 
-#   describe "#json_errors_for" do
-#     before(:each) do
-#       @object = User.new
-#       @object.valid?
-#     end
-# 
-#     it "should return a given object's errors formatted as JSON" do
-#       # FIXME: make less brittle in terms of whitespace
-#       expected = "'user': { 'password': ['can\\'t be blank'],\n'email': ['can\\'t be blank',\n'is invalid'] }"
-# 
-#       helper.json_errors_for(@object).should == expected
-#     end
-#   end
-# end
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe JsonHelper do
+  describe "#object" do
+    before(:each) do
+      helper.instance_variable_set(:@foo, 'bar')
+      helper.instance_variable_set(:@object_name, 'foo')
+    end
+
+    it "should return the object referenced by @object_name" do
+      helper.object.should == 'bar'
+    end
+  end
+
+  describe "#json_errors_for" do
+    before(:each) do
+      @object = User.new
+      @object.valid?
+    end
+
+    it "should return a given object's errors formatted as JSON" do
+      # FIXME: make less brittle in terms of whitespace
+      expected = "'user': { 'email': ['can\\'t be blank'],\n'password': ['can\\'t be blank'],\n }"
+
+      helper.json_errors_for(@object).should == expected
+    end
+  end
+end
--- spec/helpers/workplaces_helper_spec.rb
+++ spec/helpers/workplaces_helper_spec.rb
@@ -1,29 +1,27 @@
-# FIXME: helper specs don't work with rspec 2 yet ...
-#
-# require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-# 
-# describe WorkplacesHelper do
-#   describe "#requirement_list" do
-#     before(:each) do
-#       @cook_qualification = Qualification.new(:name => 'Cook')
-#       @receptionist_qualification = Qualification.new(:name => 'Receptionist')
-#       @workplace = Workplace.new
-#       @workplace.workplace_requirements.build([
-#         { :qualification => @cook_qualification, :quantity => 3 },
-#         { :qualification => @receptionist_qualification, :quantity => 2 }
-#       ])
-#     end
-# 
-#     it "should return a list of all a given workplace's requirements (truncated to 20 characters by default)" do
-#       helper.requirement_list(@workplace).should == '3x Cook, 2x Recep...'
-#     end
-# 
-#     it "should return a list of all a given workplace's requirements (truncated)" do
-#       helper.requirement_list(@workplace, :length => 15).should == '3x Cook, 2x ...'
-#     end
-# 
-#     it "should return a list of all a given workplace's requirements (not truncated)" do
-#       helper.requirement_list(@workplace, :truncate => false).should == '3x Cook, 2x Receptionist'
-#     end
-#   end
-# end
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe WorkplacesHelper do
+  describe "#requirement_list" do
+    before(:each) do
+      @cook_qualification = Qualification.new(:name => 'Cook')
+      @receptionist_qualification = Qualification.new(:name => 'Receptionist')
+      @workplace = Workplace.new
+      @workplace.workplace_requirements.build([
+        { :qualification => @cook_qualification, :quantity => 3 },
+        { :qualification => @receptionist_qualification, :quantity => 2 }
+      ])
+    end
+
+    it "should return a list of all a given workplace's requirements (truncated to 20 characters by default)" do
+      helper.requirement_list(@workplace).should == '3x Cook, 2x Recep...'
+    end
+
+    it "should return a list of all a given workplace's requirements (truncated)" do
+      helper.requirement_list(@workplace, :length => 15).should == '3x Cook, 2x ...'
+    end
+
+    it "should return a list of all a given workplace's requirements (not truncated)" do
+      helper.requirement_list(@workplace, :truncate => false).should == '3x Cook, 2x Receptionist'
+    end
+  end
+end
