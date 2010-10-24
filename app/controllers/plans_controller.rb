fc8ab062f5b0630bfc2cd18079ba4ab6726947f1
Fritz Thielemann <fritz.thielemann@gmail.com>
2010-10-24 16:41:34 +0200

    fix prawnto issues

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -19,8 +19,7 @@ class PlansController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = generate_pdf(@plan)
-        send_file(pdf.path, :disposition => 'inline')
+        prawnto :filename => "plan_#{@plan.id}.pdf", :prawn => {:page_size => 'A4'}
       end
     end
   end
@@ -100,34 +99,4 @@ class PlansController < ApplicationController
       @qualifications = current_account.qualifications
     end
 
-    # TODO: refactor, test
-    def generate_pdf(plan)
-#      Prawn::Document.generate("plan_#{plan.id}.pdf", :page_size => 'A4') do |pdf|
-      prawnto :filename => "plan_#{plan.id}.pdf", :prawn => {:page_size => 'A4'} do |pdf|
-        pdf.text "Plan: #{plan.name}", :size => 18
-        pdf.text t(:plan_from_to, :start_date => l(plan.start_date), :end_date => l(plan.end_date))
-
-        plan.days.each do |day|
-          pdf.pad_top 20 do
-            pdf.text l(day, :format => "%A, %d. %B %y")
-
-            shifts = Array(plan.shifts.by_day[day])
-            data = shifts.group_by(&:workplace).collect do |workplace, shifts|
-              shifts.map! do |shift|
-                from_to = t(:plan_from_to, :start_date => l(shift.start, :format => :time), :end_date => l(shift.end, :format => :time))
-                employees = shift.requirements.map(&:assignee_and_qualification).compact.join(', ')
-                "#{from_to}: #{employees.present? ? employees : t(:nobody)}"
-              end
-              [workplace.name, shifts.join("\n")]
-            end
-
-            if data.present?
-              pdf.table data, :border_style => :grid
-            else
-              pdf.text t(:no_shifts_for_this_day)
-            end
-          end
-        end
-      end
-    end
 end
