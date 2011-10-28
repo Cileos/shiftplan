59206b7569c6a7631050e487fc85142c3658c582
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-28 13:04:39 +0200

    generate rails 3.1.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shiftplan::Application.config.secret_token = '56c1abb6e277dd2828001c0de92b41bd2d3f6aee559603b00ef794431afe495baefaace91cec1c6c9b59029c49b59daee8f45b8a79a1e005d2160937482304c0'
