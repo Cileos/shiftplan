31ad75140f0e1b834bbe4cac27a585448594f8f0
Niklas Hofer <niklas+dev@lanpartei.de>
2011-11-28 18:55:37 +0100

    our domain for now will be shiftplan.de

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,5 @@ Shiftplan::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'shiftplan.com' }
+  config.action_mailer.default_url_options = { :host => 'shiftplan.de' }
 end
