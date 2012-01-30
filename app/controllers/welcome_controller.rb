09b7380f2f85974ba5090e6ba64ebd0c0e3b6a52
Niklas Hofer <niklas+dev@lanpartei.de>
2012-01-30 14:53:58 +0100

    create a Planner's default organization automatically

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,15 +10,6 @@ class WelcomeController < ApplicationController
 
   private
 
-  before_filter :create_default_organization, :only => :dashboard
-  def create_default_organization
-    if current_user.is_planner?
-      unless current_user.organization.present?
-        current_user.organizations.create!
-      end
-    end
-  end
-
   # TODO solve this with cancan or similar authorization framework
   def must_be_planner
     redirect_to root_path unless current_user.is_planner?
