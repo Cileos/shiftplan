9cd7d9f309f9ee669690e32783584d29a53f1d3a
Niklas Hofer <niklas+dev@lanpartei.de>
2012-02-14 19:12:39 +0100

    reload routes on eac spork run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,8 @@ Spork.each_run do
   # TODO move this to #prefork when the test suite is stable
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root/'spec'/'support'/'**'/'*.rb'].each {|f| require f}
   FactoryGirl.reload
   I18n.reload!
+  load Rails.root/'config'/'routes.rb'
 end
