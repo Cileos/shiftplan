e7562c9fe904eca25c613ee426259991d941b0fd
Niklas Hofer <niklas+dev@lanpartei.de>
2012-02-15 17:59:04 +0100

    pause step (just for linux for now)

--- features/step_definitions/development_steps.rb
+++ features/step_definitions/development_steps.rb
@@ -1,4 +1,10 @@
-When /^I pry/ do
-  binding.pry
+When /^I pause$/ do
+  STDERR.puts "Pausing..."
+  if system(%Q~zenity --question --text="Paused. Want to pry?"~)
+    binding.pry
+  end
 end
 
+When /^nothing$/ do
+  # for scenario outlines
+end
