c64a2fae53f3d104c963296b5ece40a54b2365b1
Niklas Hofer <niklas+dev@lanpartei.de>
2012-02-20 04:58:01 +0100

    test User role predicates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe User do
+
+  describe 'having role planner' do
+    let(:user) { Factory.build :planner }
+
+    it { user.should be_a_planner }
+    it { user.should be_role('planner') }
+  end
 end
