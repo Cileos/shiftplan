1371ca3bfc11762d756d7cb222a24b287fc1ec8f
Niklas Hofer <niklas+dev@lanpartei.de>
2012-02-21 20:00:44 +0100

    merge "I pause" with "try pry" step

--- features/step_definitions/development_steps.rb
+++ features/step_definitions/development_steps.rb
@@ -1,6 +1,11 @@
-When /^I pause$/ do
+When /^I pause|try pry|I pry|I debug$/ do
   STDERR.puts "Pausing..."
-  if system(%Q~zenity --question --text="Paused. Want to pry?"~)
+  if system(%Q~which zenity~)
+    if system(%Q~zenity --question --text="Paused. Want to pry?"~)
+      binding.pry
+    end
+  else
+    # no zenity installed, pry without asking
     binding.pry
   end
 end
