022a747a145c924f93467e42300421dc82d1f975
Niklas Hofer <niklas+dev@lanpartei.de>
2012-03-20 14:50:09 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120314145221) do
+ActiveRecord::Schema.define(:version => 20120316140459) do
 
   create_table "employees", :force => true do |t|
     t.string   "first_name"
