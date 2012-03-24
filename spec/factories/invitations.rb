e5fcd7c40f7525d570c9073c86d570fec195a3b7
Raphaela Wrede <rw@cileos.com>
2012-03-24 11:31:29 +0100

    update seeds

--- spec/factories/invitations.rb
+++ spec/factories/invitations.rb
@@ -2,12 +2,11 @@
 
 FactoryGirl.define do
   factory :invitation do
-    invitation_token "MyString"
-    invitation_sent_at "2012-03-23 12:40:44"
-    invitation_accepted_at "2012-03-23 12:40:44"
-    invited_by_id 1
-    organization_id 1
-    employee_id 1
-    user_id 1
+    token "mytoken"
+    sent_at "2012-03-23 12:40:44"
+    accepted_at "2012-03-23 12:41:44"
+    inviter :factory => :employee
+    organization
+    employee
   end
 end
