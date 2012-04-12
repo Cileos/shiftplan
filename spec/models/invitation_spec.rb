3103bfd34dc049331ab01ea2077fc0c5370d44e5
Raphaela Wrede <rw@cileos.com>
2012-04-12 15:32:51 +0200

    add spec for associating the invitation's employee with the invitation's user on save

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Invitation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associate employee with user' do
+    it 'should link the employee to the user on save if user is set' do
+      employee = Factory :employee
+      user = Factory :user
+      invitation = Factory(:invitation, employee: employee, organization: Factory(:organization))
+
+      employee.user.should be_nil
+      user.employees.should be_empty
+
+      invitation.user = user
+      invitation.save!
+
+      employee.reload.user.should eql(user)
+      user.reload.employees.should include(employee)
+    end
+  end
 end
