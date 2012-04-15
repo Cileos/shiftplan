ce539359d2a985df6f34a041fd1c934797afcadd
Raphaela Wrede <rw@cileos.com>
2012-04-15 09:57:36 +0200

    remove not used methods from invitation mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,22 +1,9 @@
 class InvitationMailer < ActionMailer::Base
-
   default :from => "info@shiftplan.de"
   default :charset => "UTF-8"
 
   def invitation(invitation)
     @invitation = invitation
     mail :to => invitation.email, :subject => t(:'mailer.invitation.subject')
-  end
-
-  def salutation(order)
-    t(:"order_mailer.confirmation.header.salutation.#{order.billing_address.title}",
-      :first_name => order.billing_address.first_name, :name => order.billing_address.name)
-  end
-  helper_method :salutation
-
-  def image_filetype(thumb)
-    # Call inspect on thumb.path to ensure that the right filetype can be calculated for
-    # paths including whitespace. (inspect will surround the path with ")
-    `file --mime-type #{thumb.path.inspect}`.chomp.split.last
   end
 end
