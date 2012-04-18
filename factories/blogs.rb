092ccae09f20705b7fdc7c8cf91f03272738a06d
Raphaela Wrede <rw@cileos.com>
2012-04-18 16:49:35 +0200

    make blog and post factories generate valid records

--- factories/blogs.rb
+++ factories/blogs.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :blog do
+    organization
     sequence(:title) { |i| "Blog title #{i.ordinalize}" }
   end
   factory :post do
+    blog
+    association :author, factory: :employee
     sequence(:title) { |i| "Post title #{i.ordinalize}" }
     sequence(:body)  { |i| "Post body #{i.ordinalize}" }
     published_at { Time.now }
