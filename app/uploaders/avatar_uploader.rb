efe92886e65a2bac06fb65ef2e63379e58e38fdf
Raphaela Wrede <rw@cileos.com>
2012-05-08 17:43:31 +0200

    add a tiny version for the avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -53,6 +53,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [50, 50]
   end
 
+  version :tiny do
+    process :resize_to_fit => [27, 27]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
