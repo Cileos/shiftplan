0c9f183cd65c9a19824cef29dbf13561f2d1db71
Niklas Hofer <niklas+dev@lanpartei.de>
2012-05-09 17:44:28 +0200

    map it, text it, strip it, work it

--- features/support/table_helpers.rb
+++ features/support/table_helpers.rb
@@ -1,12 +1,12 @@
 # TODO move this to selectors.rb
 def column_index_for(column_label)
-  columns = page.all('thead tr th').map(&:text)
+  columns = page.all('thead tr th').map(&:text).map(&:strip)
   columns.should include(column_label)
   columns.index(column_label)
 end
 
 def row_index_for(row_label)
-  rows = page.all("tbody th").map(&:text)
+  rows = page.all("tbody th").map(&:text).map(&:strip)
   rows.should include(row_label)
   rows.index(row_label)
 end
