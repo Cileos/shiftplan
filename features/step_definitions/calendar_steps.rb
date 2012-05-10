08747ce20c1cbb2a31b861ec682657663b0b2506
Niklas Hofer <niklas+dev@lanpartei.de>
2012-05-10 20:35:51 +0200

    (cucumber) ignore additional elements in calendar

--- features/step_definitions/calendar_steps.rb
+++ features/step_definitions/calendar_steps.rb
@@ -51,7 +51,14 @@ end
 # FIXME can only match the whole calendar
 Then /^I should see the following calendar:$/ do |expected|
   actual = find(selector_for('the calendar')).all("tr").map do |tr|
-    tr.all('th, td').map(&:text).map {|text| text.strip.gsub(/\s+/, ' ') }
+    tr.all('th, td').map do |cell|
+      if cell.tag_name == 'th' or cell.all('*').empty? # a text-only cell or header
+        cell.text
+      else
+        # ignore the other elements, for example the comment links or avatars
+        cell.all('abbr,span').map(&:text).join(' ')
+      end.strip.gsub(/\s+/, ' ')
+    end
   end
   expected.diff! actual
 end
