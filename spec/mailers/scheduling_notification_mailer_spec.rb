2de3fa949a632762aec60e8b85a2038ba400c33f
Raphaela Wrede <rw@cileos.com>
2012-05-11 12:00:16 +0200

    spec the email body
    - add spec for email body "should have appropriate information about the scheduling that was commented on in the email body"
    - add spec for email body "should have appropriate information about the scheduling for which an answer on a comment was written in the email body"
    - add spec for email body "should always should make clear that it is my own scheduling or that I commented before"

--- spec/mailers/scheduling_notification_mailer_spec.rb
+++ spec/mailers/scheduling_notification_mailer_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe SchedulingNotificationMailer do
+
   describe 'new_comment' do
 
     before :each do
@@ -194,6 +195,59 @@ describe SchedulingNotificationMailer do
         owner_2_mails.first.body.should include "Owner Simpson hat eine Schicht von Homer Simpson am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) kommentiert:"
       end
 
+      it 'should have appropriate information about the scheduling for which an answer on a comment was written in the email body' do
+        barts_comment = Comment.build_from(@scheduling_for_homer, @employee_bart,
+          body: 'Homer, denk bitte daran, bei Feierabend den Reaktor zu putzen')
+        barts_comment.save!
+
+        ActionMailer::Base.deliveries = []
+
+        owners_comment = Comment.build_from(@scheduling_for_homer, @employee_owner,
+          body: 'Homer, denk bitte daran, bei Feierabend die Fenster zu schliessen',
+          parent: barts_comment)
+        owners_comment.save!
+
+        ActionMailer::Base.deliveries.count.should == 4
+
+        # as bart commented before, he receives a notification
+        barts_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['bart.simpson@shiftplan.local'] }
+        barts_mails.first.body.should include "Owner Simpson hat auf Ihren Kommentar zu einer Schicht von Homer Simpson am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet:"
+
+        # it's homer's scheduling that was commented
+        homers_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['homer.simpson@shiftplan.local'] }
+        homers_mails.first.body.should include "Owner Simpson hat auf einen Kommentar zu einer Ihrer Schichten am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet:"
+
+        planners_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['planner@shiftplan.local'] }
+        planners_mails.first.body.should include "Owner Simpson hat auf einen Kommentar zu einer Schicht von Homer Simpson am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet:"
+
+        owner_2_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['owner_2@shiftplan.local'] }
+        owner_2_mails.first.body.should include "Owner Simpson hat auf einen Kommentar zu einer Schicht von Homer Simpson am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet:"
+      end
+
+      it 'should always should make clear that it is my own scheduling or that I commented before' do
+        barts_comment = Comment.build_from(@scheduling_for_homer, @employee_bart,
+          body: 'Homer, denk bitte daran, bei Feierabend den Reaktor zu putzen')
+        barts_comment.save!
+
+        # homer comments his own shift
+        homers_comment = Comment.build_from(@scheduling_for_homer, @employee_homer,
+          body: 'Owner Simpson, bitte stell mir den Besen in die Putzkammer')
+        barts_comment.save!
+
+        # owner writes answer for homers comment
+        owners_comment = Comment.build_from(@scheduling_for_homer, @employee_owner,
+          body: 'Homer, habe ich gerade erledigt.',
+          parent: homers_comment)
+        owners_comment.save!
+
+        # as bart commented before, he receives a notification
+        barts_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['bart.simpson@shiftplan.local'] }
+        barts_mails.last.body.should include "Owner Simpson hat auf einen Kommentar zu einer Schicht von Homer Simpson am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet. Diese Schicht haben Sie ebenfalls kommentiert:"
+
+        # it's homer's scheduling that was commented
+        homers_mails = ActionMailer::Base.deliveries.select { |mail| mail.to == ['homer.simpson@shiftplan.local'] }
+        homers_mails.last.body.should include "Owner Simpson hat auf Ihren Kommentar zu einer Ihrer Schichten am Freitag, den 21.12.2012 (3-5 Reaktor putzen [Rp]) geantwortet:"
+      end
     end
 
   end
