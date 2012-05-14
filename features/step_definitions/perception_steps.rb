4a4e9f1d6813f46b33ddb783377d16d9961a9462
tatze <mdz.git@emtrax.net>
2012-05-14 16:12:10 +0200

    ignore links and buttons in employee table

--- features/step_definitions/perception_steps.rb
+++ features/step_definitions/perception_steps.rb
@@ -47,7 +47,18 @@ end
 Then /^I should see the following table of (.+):$/ do |plural, expected|
   # table is a Cucumber::Ast::Table
   actual = find("table##{plural}").all('tr').map do |tr|
-    tr.all('th,td').map(&:text).map(&:strip)
+    # tr.all('th,td').map(&:text).map(&:strip)
+    tr.all('th, td').map do |cell|
+    if cell.tag_name == 'th' or cell.all('*').empty? # a text-only cell ader
+      cell.text
+    else # remove the text of all included buttons and links, they gonna be clicked anyway
+      text = cell.text
+      cell.all('a,button').each do |e|
+        text = text.sub(e.text, '')
+      end
+      text
+    end.strip.squeeze(' ')
+    end
   end
   expected.diff! actual
 end
