84e38c3c0709b8f27ed043c1aef348f4c5bdf115
Niklas Hofer <niklas+dev@lanpartei.de>
2012-05-15 13:23:48 +0200

    (m) remove duplication

--- app/mailers/post_notification_mailer.rb
+++ app/mailers/post_notification_mailer.rb
@@ -2,15 +2,12 @@ class PostNotificationMailer < ActionMailer::Base
   default charset: 'UTF-8'
   default from: 'Shiftplan <no-reply@shiftplan.de>'
 
-  def new_post(notification)
+  def new_notification(notification)
     @notification = notification
     @post         = notification.post
     mail to: notification.employee.user.email, subject: notification.subject
   end
 
-  def new_comment(notification)
-    @notification = notification
-    @post         = notification.post
-    mail to: notification.employee.user.email, subject: notification.subject
-  end
+  alias_method :new_post, :new_notification
+  alias_method :new_icomment, :new_notification
 end
