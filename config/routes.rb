b1e33fb289a3a322bffe9e0b94253adbc1c3fbab
Raphaela Wrede <rw@cileos.com>
2012-06-19 15:48:26 +0200

    match http methods to controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Shiftplan::Application.routes.draw do
 
-  match 'invitation/accept'    => 'accept_invitations#accept',  :as => :accept_invitation
-  match 'invitation/confirm'   => 'accept_invitations#confirm', :as => :confirm_invitation
-  match 'email_change/accept'  => 'email_change#accept',        :as => :accept_email_change
-  match 'email_change/confirm' => 'email_change#confirm',       :as => :confirm_email_change
+  get 'invitation/accept'    => 'accept_invitations#accept',  :as => :accept_invitation
+  put 'invitation/confirm'   => 'accept_invitations#confirm', :as => :confirm_invitation
+  get 'email_change/accept'  => 'email_change#accept',        :as => :accept_email_change
+  put 'email_change/confirm' => 'email_change#confirm',       :as => :confirm_email_change
 
   resources :organizations do
     resources :plans do
