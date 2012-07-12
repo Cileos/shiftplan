845e833aaccb5017b626c31359a3abe2bc4127f0
Niklas Hofer <niklas+dev@lanpartei.de>
2012-07-12 18:54:31 +0200

    developer can use hash.should be_hash_matching

---
+++ spec/support/hash_matching.rb
@@ -0,0 +1,27 @@
+require 'diff_matcher'
+
+module RSpec
+  module Matchers
+    class BeMatching
+      attr_reader :expected
+
+      def initialize(expected, opts)
+        @expected = expected
+        @opts = opts.update(:color_enabled=>RSpec::configuration.color_enabled?)
+      end
+
+      def matches?(actual)
+        @difference = DiffMatcher::Difference.new(expected, actual, @opts)
+        @difference.matching?
+      end
+
+      def failure_message_for_should
+        @difference.to_s
+      end
+    end
+
+    def be_hash_matching(expected, opts={})
+      Matchers::BeMatching.new(expected, opts)
+    end
+  end
+end
