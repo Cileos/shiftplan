36bf45ea2079ecad035a995597893b270ec86065
Niklas Hofer <niklas+dev@lanpartei.de>
2012-08-09 16:43:23 +0200

    always show dashboard after signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    params[:return_to] || dashboard_path
+    params[:return_to] || dynamic_dashboard_path
   end
+
+  def dynamic_dashboard_path
+    if current_user.organizations.count == 1
+      current_user.organizations.first
+    else
+      dashboard_path
+    end
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   skip_authorization_check only: :landing
   no_authentication_required only: :landing
   before_filter :authorize_user, only: :dashboard
-  before_filter :check_for_multiple_organizations, only: :dashboard
+  before_filter :redirect_to_dashboard_if_signed_in, only: :landing
 
   def landing
   end
@@ -16,10 +16,10 @@ class WelcomeController < ApplicationController
     authorize! :dashboard, current_user
   end
 
-  def check_for_multiple_organizations
-    if current_user.organizations.count == 1
-      flash.keep
-      redirect_to [current_user.organizations.first]
+  def redirect_to_dashboard_if_signed_in
+    if user_signed_in?
+      redirect_to dashboard_path
     end
   end
+
 end
