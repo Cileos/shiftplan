d9327c6aacedc5dd48914895581f58e5e6cbd571
Raphaela Wrede <rw@cileos.com>
2012-09-12 17:52:15 +0200

    use rspec shared_examples in plan spec to make it shorter

--- spec/abilities/plan_spec.rb
+++ spec/abilities/plan_spec.rb
@@ -1,6 +1,41 @@
 require 'spec_helper'
 require "cancan/matchers"
 
+shared_examples "a planner for own accounts" do
+  it "should be able to read plans" do
+    should be_able_to(:read, create(:plan, organization: organization))
+  end
+  it "should be able to update plans" do
+    should be_able_to(:update, create(:plan, organization: organization))
+  end
+  it "should be able to create plans" do
+    should be_able_to(:create, build(:plan, organization: organization))
+  end
+  it "should be able to destroy plans" do
+    should be_able_to(:destroy, create(:plan, organization: organization))
+  end
+end
+
+shared_examples "a planner for other accounts" do
+  it "should not be able to read plans" do
+    should_not be_able_to(:read, create(:plan, organization: different_organization))
+  end
+  it "should not be able to update plans" do
+    should_not be_able_to(:update, create(:plan, organization: different_organization))
+  end
+  it "should not be able to create plans" do
+    should_not be_able_to(:create, build(:plan, organization: different_organization))
+  end
+  it "should not be able to destroy plans" do
+    should_not be_able_to(:destroy, create(:plan, organization: different_organization))
+  end
+end
+
+shared_examples "a planning planner" do
+  it_behaves_like "a planner for own accounts"
+  it_behaves_like "a planner for other accounts"
+end
+
 describe "Plan permissions:" do
   subject { ability }
   let(:ability) { Ability.new(user) }
@@ -18,95 +53,30 @@ describe "Plan permissions:" do
     user.current_employee = employee if employee
   end
 
-  context "As an owner" do
+  context "An owner" do
     let(:employee) { create(:employee_owner, account: account, user: user) }
 
-    context "for organizations of different accounts" do
-      it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: different_organization))
-      end
-
-      it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
-      end
-    end
-
-    context "for own organizations" do
-      it "should be able to read plans" do
-        should be_able_to(:read, create(:plan, organization: organization))
-      end
-
-      it "should be able to update plans" do
-        should be_able_to(:update, create(:plan, organization: organization))
-      end
-
-      it "should be able to create plans" do
-        should be_able_to(:create, build(:plan, organization: organization))
-      end
-
-      it "should be able to destroy plans" do
-        should be_able_to(:destroy, create(:plan, organization: organization))
-      end
-    end
+    it_behaves_like "a planning planner"
   end
 
   context "As a planner" do
     let(:employee) { create(:employee_planner, account: account, user: user) }
 
-    context "for organizations of different accounts" do
-      it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: different_organization))
-      end
-
-      it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
-      end
-    end
-
-    context "for own organizations" do
-      it "should be able to read plans" do
-        should be_able_to(:read, create(:plan, organization: organization))
-      end
-
-      it "should be able to update plans" do
-        should be_able_to(:update, create(:plan, organization: organization))
-      end
-
-      it "should be able to create plans" do
-        should be_able_to(:create, build(:plan, organization: organization))
-      end
-
-      it "should be able to destroy plans" do
-        should be_able_to(:destroy, create(:plan, organization: organization))
-      end
-    end
+    it_behaves_like "a planning planner"
   end
 
   context "As an employee" do
     let(:employee)   { create(:employee, account: account, user: user) }
+    # An "normal" employee needs a membership for an organization to do things.
+    # This is different from planners or owners which do not need a membership but
+    # just the role "planner" or "owner" and belong to the acccount.
     let(:membership) { create(:membership, employee: employee, organization: organization) }
 
     before(:each) do
       membership
     end
 
-    context "for organizations of which (s)he is member of" do
+    context "for own organizations" do
       it "should be able to read plans" do
         should be_able_to(:read, create(:plan, organization: organization))
       end
@@ -124,60 +94,18 @@ describe "Plan permissions:" do
       end
     end
 
-    context "for organizations of which (s)he is not member of" do
-      it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: another_organization))
-      end
-
-      it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: another_organization))
-      end
-
-      it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: another_organization))
-      end
-
-      it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: another_organization))
-      end
+    context "for other organizations" do
+      it_behaves_like "a planner for other accounts"
     end
 
     context "for organizations of different accounts" do
-      it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: different_organization))
-      end
-
-      it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: different_organization))
-      end
-
-      it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
-      end
+      it_behaves_like "a planner for other accounts"
     end
   end
 
   context "As an user without employee(not possible but for the case)" do
     let(:employee) { nil }
 
-    it "should not be able to read plans" do
-      should_not be_able_to(:read, create(:plan, organization: organization))
-    end
-
-    it "should not be able to destroy plans" do
-      should_not be_able_to(:destroy, create(:plan, organization: organization))
-    end
-
-    it "should not be able to create plans" do
-      should_not be_able_to(:create, create(:plan, organization: organization))
-    end
-
-    it "should not be able to update plans" do
-      should_not be_able_to(:update, create(:plan, organization: organization))
-    end
+    it_behaves_like "a planner for other accounts"
   end
 end
