79fa1ff121f4b3b4cac973ba33aee8c194545414
Raphaela Wrede <rw@cileos.com>
2012-09-12 13:17:26 +0200

    fix plan spec after data model and abilities have been changed

--- spec/abilities/plan_spec.rb
+++ spec/abilities/plan_spec.rb
@@ -5,8 +5,13 @@ describe "Plan permissions:" do
   subject { ability }
   let(:ability) { Ability.new(user) }
   let(:user) { create(:user) }
-  let(:organization) { create(:organization) }
-  let(:another_organization) { create(:organization) }
+
+  let(:account) { create(:account) }
+  let(:organization) { create(:organization, account: account) }
+  let(:another_organization) { create(:organization, account: account) }
+
+  let(:different_account) { create(:account) }
+  let(:different_organization) { create(:organization, account: different_account) }
 
   before(:each) do
     # simulate before_filter :set_current_employee
@@ -14,53 +19,98 @@ describe "Plan permissions:" do
   end
 
   context "As an owner" do
-    let(:employee) { create(:employee_owner, organization: organization, user: user) }
+    let(:employee) { create(:employee_owner, account: account, user: user) }
 
-    context "for other organizations" do
+    context "for organizations of different accounts" do
       it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: another_organization))
+        should_not be_able_to(:read, create(:plan, organization: different_organization))
       end
 
       it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: another_organization))
+        should_not be_able_to(:update, create(:plan, organization: different_organization))
       end
 
       it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: another_organization))
+        should_not be_able_to(:create, build(:plan, organization: different_organization))
       end
 
       it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: another_organization))
+        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
+      end
+    end
+
+    context "for own organizations" do
+      it "should be able to read plans" do
+        should be_able_to(:read, create(:plan, organization: organization))
+      end
+
+      it "should be able to update plans" do
+        should be_able_to(:update, create(:plan, organization: organization))
+      end
+
+      it "should be able to create plans" do
+        should be_able_to(:create, build(:plan, organization: organization))
+      end
+
+      it "should be able to destroy plans" do
+        should be_able_to(:destroy, create(:plan, organization: organization))
       end
     end
   end
 
   context "As a planner" do
-    let(:employee) { create(:employee, organization: organization, user: user) }
+    let(:employee) { create(:employee_planner, account: account, user: user) }
 
-    context "for other organizations" do
+    context "for organizations of different accounts" do
       it "should not be able to read plans" do
-        should_not be_able_to(:read, create(:plan, organization: another_organization))
+        should_not be_able_to(:read, create(:plan, organization: different_organization))
       end
 
       it "should not be able to update plans" do
-        should_not be_able_to(:update, create(:plan, organization: another_organization))
+        should_not be_able_to(:update, create(:plan, organization: different_organization))
       end
 
       it "should not be able to create plans" do
-        should_not be_able_to(:create, build(:plan, organization: another_organization))
+        should_not be_able_to(:create, build(:plan, organization: different_organization))
       end
 
       it "should not be able to destroy plans" do
-        should_not be_able_to(:destroy, create(:plan, organization: another_organization))
+        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
+      end
+    end
+
+    context "for own organizations" do
+      it "should be able to read plans" do
+        should be_able_to(:read, create(:plan, organization: organization))
+      end
+
+      it "should be able to update plans" do
+        should be_able_to(:update, create(:plan, organization: organization))
+      end
+
+      it "should be able to create plans" do
+        should be_able_to(:create, build(:plan, organization: organization))
+      end
+
+      it "should be able to destroy plans" do
+        should be_able_to(:destroy, create(:plan, organization: organization))
       end
     end
   end
 
   context "As an employee" do
-    let(:employee) { create(:employee, organization: organization, user: user) }
+    let(:employee)   { create(:employee, account: account, user: user) }
+    let(:membership) { create(:membership, employee: employee, organization: organization) }
 
-    context "for own organization" do
+    before(:each) do
+      membership
+    end
+
+    context "for organizations of which (s)he is member of" do
+      it "should be able to read plans" do
+        should be_able_to(:read, create(:plan, organization: organization))
+      end
+
       it "should not be able to destroy plans" do
         should_not be_able_to(:destroy, create(:plan, organization: organization))
       end
@@ -74,7 +124,7 @@ describe "Plan permissions:" do
       end
     end
 
-    context "for other organizations" do
+    context "for organizations of which (s)he is not member of" do
       it "should not be able to read plans" do
         should_not be_able_to(:read, create(:plan, organization: another_organization))
       end
@@ -89,6 +139,24 @@ describe "Plan permissions:" do
 
       it "should not be able to destroy plans" do
         should_not be_able_to(:destroy, create(:plan, organization: another_organization))
+      end
+    end
+
+    context "for organizations of different accounts" do
+      it "should not be able to read plans" do
+        should_not be_able_to(:read, create(:plan, organization: different_organization))
+      end
+
+      it "should not be able to update plans" do
+        should_not be_able_to(:update, create(:plan, organization: different_organization))
+      end
+
+      it "should not be able to create plans" do
+        should_not be_able_to(:create, build(:plan, organization: different_organization))
+      end
+
+      it "should not be able to destroy plans" do
+        should_not be_able_to(:destroy, create(:plan, organization: different_organization))
       end
     end
   end
