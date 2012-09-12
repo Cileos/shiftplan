c93b9be2d0d7775a9b0e158ce6ca57d62e09cec0
tatze <mdz.git@emtrax.net>
2012-09-12 18:28:48 +0200

    disable 'switch_calendar_layout' for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 //= require comments
 //= require teams
 //= require 'calendar/cursor'
-//= require 'calendar/switch_calendar_layout'
 //= require 'calendar/vertical_positioning'
 //= require_tree './editors'
 //= require mailcheck
