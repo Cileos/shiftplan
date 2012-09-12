46bcbab4c72c59d6e680e3677ab41199aac24e81
Raphaela Wrede <rw@cileos.com>
2012-09-12 17:16:11 +0200

    fix ability spec for comments

--- spec/abilities/comment_spec.rb
+++ spec/abilities/comment_spec.rb
@@ -1,6 +1,102 @@
 require 'spec_helper'
 require "cancan/matchers"
 
+shared_examples "an employee who can read comments" do
+  it "I should be able to read comments on posts" do
+    comment = Comment.build_from(post, some_employee, body: 'some text')
+    should be_able_to(:read, comment)
+  end
+  it "I should be able to read comments on schedulings" do
+    comment = Comment.build_from(scheduling, some_employee, body: 'some text')
+    should be_able_to(:read, comment)
+  end
+end
+
+shared_examples "an employee who can create comments" do
+  it "I should be able to create comments on posts" do
+    comment = Comment.build_from(post, employee, body: 'some text')
+    should be_able_to(:create, comment)
+  end
+  it "I should be able to create comments on schedulings" do
+    comment = Comment.build_from(scheduling, employee, body: 'some text')
+    should be_able_to(:create, comment)
+  end
+end
+
+shared_examples "an employee who can destroy his own comments" do
+  it "I should be able to destroy my own comments on posts" do
+    comment = Comment.build_from(post, employee, body: 'some text')
+    should be_able_to(:destroy, comment)
+  end
+  it "I should be able to destroy my own comments on schedulings" do
+    comment = Comment.build_from(scheduling, employee, body: 'some text')
+    should be_able_to(:destroy, comment)
+  end
+end
+
+shared_examples "an employee who can not destroy comments which are not his own" do
+  it "I should not be able to destroy comments on posts having an author different than me" do
+    comment = Comment.build_from(post, some_employee, body: 'some text')
+    should_not be_able_to(:destroy, comment)
+  end
+  it "I should not be able to destroy comments on schedulings having an author different than me" do
+    comment = Comment.build_from(scheduling, some_employee, body: 'some text')
+    should_not be_able_to(:destroy, comment)
+  end
+end
+
+shared_examples "an employee who can not update comments" do
+  # updating: comments are not updatable at the moment
+  it "I should not be able to update comments on posts, even my own" do
+    comment = Comment.build_from(post, employee, body: 'some text')
+    should_not be_able_to(:update, comment)
+  end
+  it "I should not be able to update comments on schedulings, even my own" do
+    comment = Comment.build_from(scheduling, employee, body: 'some text')
+    should_not be_able_to(:update, comment)
+  end
+end
+
+shared_examples "an employee who can not read comments" do
+  it "I should not be able to read comments on posts" do
+    # other_post belongs to other account
+    comment = Comment.build_from(other_post, other_employee, body: 'some text')
+    should_not be_able_to(:read, comment)
+  end
+
+  it "I should not be able to read comments on schedulings" do
+    # other_scheduling belongs to other account
+    comment = Comment.build_from(other_scheduling, other_employee, body: 'some text')
+    should_not be_able_to(:read, comment)
+  end
+end
+
+shared_examples "an employee who can not create comments" do
+  it "I should not be able to create comments on posts of different accounts" do
+    comment = Comment.build_from(other_post, employee, body: 'some text')
+    should_not be_able_to(:create, comment)
+  end
+  it "I should not be able to create comments on schedulings of different accounts" do
+    comment = Comment.build_from(other_scheduling, employee, body: 'some text')
+    should_not be_able_to(:create, comment)
+  end
+end
+
+shared_examples "a commenting employee" do
+  context "for own accounts" do
+    it_behaves_like "an employee who can read comments"
+    it_behaves_like "an employee who can create comments"
+    it_behaves_like "an employee who can destroy his own comments"
+    it_behaves_like "an employee who can not destroy comments which are not his own"
+    it_behaves_like "an employee who can not update comments"
+  end
+
+  context "for other accounts" do
+    it_behaves_like "an employee who can not read comments"
+    it_behaves_like "an employee who can not create comments"
+  end
+end
+
 describe "Comment permissions:" do
   subject { ability }
   let(:ability) { Ability.new(user) }
@@ -9,7 +105,7 @@ describe "Comment permissions:" do
   let(:account) { create(:account)}
   let(:organization) { create(:organization, account: account) }
   let(:some_employee) { create(:employee, account: account) }
-  let(:membership) { create(:membership, employee: some_employee, organization: organization) }
+  let(:some_membership) { create(:membership, employee: some_employee, organization: organization) }
 
   let(:blog) { create(:blog, organization: organization) }
   let(:post) { create(:post, blog: blog) }
@@ -27,159 +123,34 @@ describe "Comment permissions:" do
   let(:other_scheduling) { create(:scheduling, plan: other_plan) }
 
   before(:each) do
-    membership
     # simulate before_filter :set_current_employee
     user.current_employee = employee if employee
   end
 
-  context "As an owner" do
+  context "An owner" do
     let(:employee) { create(:employee_owner, account: account, user: user) }
 
-    context "for own accounts" do
-      # reading
-      it "I should be able to read comments on posts" do
-        comment = Comment.build_from(post, some_employee, body: 'some text')
-        should be_able_to(:read, comment)
-      end
-      it "I should be able to read comments on schedulings" do
-        comment = Comment.build_from(scheduling, some_employee, body: 'some text')
-        should be_able_to(:read, comment)
-      end
-
-      # creating
-      it "I should be able to create comments on posts" do
-        comment = Comment.build_from(post, employee, body: 'some text')
-        should be_able_to(:create, comment)
-      end
-      it "I should be able to create comments on schedulings" do
-        comment = Comment.build_from(scheduling, employee, body: 'some text')
-        should be_able_to(:create, comment)
-      end
-
-      # destroying
-      it "I should be able to destroy my own comments on posts" do
-        comment = Comment.build_from(post, employee, body: 'some text')
-        should be_able_to(:destroy, comment)
-      end
-      it "I should be able to destroy my own comments on schedulings" do
-        comment = Comment.build_from(scheduling, employee, body: 'some text')
-        should be_able_to(:destroy, comment)
-      end
-      it "I should not be able to destroy comments on posts having an author different than me" do
-        comment = Comment.build_from(post, some_employee, body: 'some text')
-        should_not be_able_to(:destroy, comment)
-      end
-      it "I should not be able to destroy comments on schedulings having an author different than me" do
-        comment = Comment.build_from(scheduling, some_employee, body: 'some text')
-        should_not be_able_to(:destroy, comment)
-      end
-
-      # updating: comments are not updatable at the moment
-      it "I should not be able to update comments on posts, even my own" do
-        comment = Comment.build_from(post, employee, body: 'some text')
-        should_not be_able_to(:update, comment)
-      end
-      it "I should not be able to update comments on schedulings, even my own" do
-        comment = Comment.build_from(scheduling, employee, body: 'some text')
-        should_not be_able_to(:update, comment)
-      end
-    end
-
-    context "for other accounts" do
-      # reading
-      it "I should not be able to read comments on posts" do
-        comment = Comment.build_from(other_post, other_employee, body: 'some text')
-        should_not be_able_to(:read, comment)
-      end
-
-      it "I should not be able to read comments on schedulings" do
-        comment = Comment.build_from(other_scheduling, other_employee, body: 'some text')
-        should_not be_able_to(:read, comment)
-      end
-
-      # creating
-      it "I should not be able to create comments on posts of different accounts" do
-        comment = Comment.build_from(other_post, employee, body: 'some text')
-        should_not be_able_to(:create, comment)
-      end
-      it "I should not be able to create comments on schedulings of different accounts" do
-        comment = Comment.build_from(other_scheduling, employee, body: 'some text')
-        should_not be_able_to(:create, comment)
-      end
-    end
+    it_behaves_like "a commenting employee"
   end
 
-  # context "As a planner" do
-  #   let(:employee) { create(:employee, organization: organization, user: user) }
+  context "A planner" do
+    let(:employee) { create(:employee_planner, account: account, user: user) }
 
-  #   context "for my own organization" do
-  #     it "I should not be able to destroy comments having an author different than me" do
-  #       comment = Comment.build_from(post, some_employee, body: 'some text')
-  #       should_not be_able_to(:destroy, comment)
-  #     end
+    it_behaves_like "a commenting employee"
+  end
 
-  #     it "should not be able to update comments" do
-  #       comment = Comment.build_from(post, employee, body: 'some text')
-  #       should_not be_able_to(:update, comment)
-  #     end
-  #   end
+  context "An employee" do
+    let(:employee) { create(:employee, account: account, user: user) }
+    # An "normal" employee needs a membership for an organization to do things.
+    # This is different from planners or owners which do not need a membership but
+    # just the role "planner" or "owner" and belong to the acccount.
+    let(:membership) { create(:membership, employee: employee, organization: organization) }
 
-  #   context "for other organizations" do
+    before(:each) do
+      membership
+    end
 
-  #     let(:comment) { comment = Comment.build_from(another_post, other_employee, body: 'some text') }
-
-  #     it "should not be able to create comments" do
-  #       should_not be_able_to(:create, comment)
-  #     end
-
-  #     it "should not be able to read comments" do
-  #       should_not be_able_to(:read, comment)
-  #     end
-
-  #     it "should not be able to destroy comments" do
-  #       should_not be_able_to(:destroy, comment)
-  #     end
-
-  #     it "should not be able to update comments" do
-  #       should_not be_able_to(:update, comment)
-  #     end
-  #   end
-  # end
-
-  # context "As an employee" do
-  #   let(:employee) { create(:employee, organization: organization, user: user) }
-
-  #   context "for my own organization" do
-  #     it "I should not be able to destroy comments having an author different than me" do
-  #       comment = Comment.build_from(post, some_employee, body: 'some text')
-  #       should_not be_able_to(:destroy, comment)
-  #     end
-
-  #     it "should not be able to update comments" do
-  #       comment = Comment.build_from(post, employee, body: 'some text')
-  #       should_not be_able_to(:update, comment)
-  #     end
-  #   end
-
-  #   context "for other organizations" do
-
-  #     let(:comment) { comment = Comment.build_from(another_post, other_employee, body: 'some text') }
-
-  #     it "should not be able to create comments" do
-  #       should_not be_able_to(:create, comment)
-  #     end
-
-  #     it "should not be able to read comments" do
-  #       should_not be_able_to(:read, comment)
-  #     end
-
-  #     it "should not be able to destroy comments" do
-  #       should_not be_able_to(:destroy, comment)
-  #     end
-
-  #     it "should not be able to update comments" do
-  #       should_not be_able_to(:update, comment)
-  #     end
-  #   end
-  # end
+    it_behaves_like "a commenting employee"
+  end
 end
+
