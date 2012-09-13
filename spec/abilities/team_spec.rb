4678747ba27feac612a05b716376e75e9ae24bca
Raphaela Wrede <rw@cileos.com>
2012-09-13 20:30:55 +0200

    spec team abilities after the data model changed

--- spec/abilities/team_spec.rb
+++ spec/abilities/team_spec.rb
@@ -1,99 +1,92 @@
 require 'spec_helper'
 require "cancan/matchers"
 
+shared_examples  "an employee who can not create, read, update and destroy teams" do
+  it "should not be able to create teams" do
+    should_not be_able_to(:create, create(:team, organization: other_organization))
+  end
+  it "should not be able to read teams" do
+    should_not be_able_to(:read, create(:team, organization: other_organization))
+  end
+  it "should not be able to update teams" do
+    should_not be_able_to(:update, create(:team, organization: other_organization))
+  end
+  it "should not be able to destroy teams" do
+    should_not be_able_to(:destroy, create(:team, organization: other_organization))
+  end
+end
+
+shared_examples "an employee who can manage teams" do
+  context "for own accounts" do
+    it "should be able to manage teams" do
+      should be_able_to(:manage, create(:team, organization: organization))
+    end
+  end
+  context "for other accounts" do
+    it_behaves_like "an employee who can not create, read, update and destroy teams" do
+      let(:other_account) { create(:account) }
+      let(:other_organization) { create(:organization, account: other_account) }
+      let(:other_plan) { create(:plan, organization: other_organization) }
+    end
+  end
+end
+
+shared_examples "an employee who can read teams" do
+  context "for own accounts" do
+    it "should be able to read teams" do
+      should be_able_to(:read, create(:team, organization: organization))
+    end
+  end
+  context "for other accounts" do
+    it_behaves_like "an employee who can not create, read, update and destroy teams" do
+      # The employee has no membership for the other_organization to wich
+      # other_plan belongs.  Therefore he can not even read a team or
+      # this other_plan.
+      let(:other_organization) { create(:organization, account: account) }
+    end
+  end
+end
+
 describe "Team permissions:" do
   subject { ability }
   let(:ability) { Ability.new(user) }
   let(:user) { create(:user) }
-  let(:organization) { create(:organization) }
-  let(:another_organization) { create(:organization) }
+  let(:account) { create(:account) }
+  let(:organization) { create(:organization, account: account) }
+
 
   before(:each) do
     # simulate before_filter :set_current_employee
     user.current_employee = employee if employee
   end
 
-  context "As an owner" do
-    let(:employee) { create(:employee_owner, organization: organization, user: user) }
-
-    context "for other organizations" do
-      it "should not be able to read teams" do
-        should_not be_able_to(:read, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to update teams" do
-        should_not be_able_to(:update, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to create teams" do
-        should_not be_able_to(:create, build(:team, organization: another_organization))
-      end
-
-      it "should not be able to destroy teams" do
-        should_not be_able_to(:destroy, create(:team, organization: another_organization))
-      end
+  context "An owner" do
+    it_behaves_like "an employee who can manage teams" do
+      let(:employee) { create(:employee_owner, account: account, user: user) }
     end
   end
 
-  context "As a planner" do
-    let(:employee) { create(:employee, organization: organization, user: user) }
-
-    context "for other organizations" do
-      it "should not be able to read teams" do
-        should_not be_able_to(:read, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to update teams" do
-        should_not be_able_to(:update, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to create teams" do
-        should_not be_able_to(:create, build(:team, organization: another_organization))
-      end
-
-      it "should not be able to destroy teams" do
-        should_not be_able_to(:destroy, create(:team, organization: another_organization))
-      end
+  context "A planner" do
+    it_behaves_like "an employee who can manage teams" do
+      let(:employee) { create(:employee_planner, account: account, user: user) }
     end
   end
 
-  context "As an employee" do
-    let(:employee) { create(:employee, organization: organization, user: user) }
-
-    context "for own organization" do
-      it "should not be able to destroy teams" do
-        should_not be_able_to(:destroy, create(:team, organization: organization))
-      end
-
-      it "should not be able to create teams" do
-        should_not be_able_to(:create, create(:team, organization: organization))
-      end
-
-      it "should not be able to update teams" do
-        should_not be_able_to(:update, create(:team, organization: organization))
-      end
+  context "An employee" do
+    before(:each) do
+      membership
     end
 
-    context "for other organizations" do
-      it "should not be able to read teams" do
-        should_not be_able_to(:read, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to update teams" do
-        should_not be_able_to(:update, create(:team, organization: another_organization))
-      end
-
-      it "should not be able to create teams" do
-        should_not be_able_to(:create, build(:team, organization: another_organization))
-      end
-
-      it "should not be able to destroy teams" do
-        should_not be_able_to(:destroy, create(:team, organization: another_organization))
-      end
+    it_behaves_like "an employee who can read teams" do
+      let(:employee) { create(:employee, account: account, user: user) }
+      # An "normal" employee needs a membership for an organization to do things.
+      # This is different from planners or owners which do not need a membership but
+      # just the role "planner" or "owner" and belong to the acccount.
+      let(:membership) { create(:membership, employee: employee, organization: organization) }
     end
   end
 
-  context "As an user without employee(not possible but for the case)" do
+  context "An user without employee(not possible but for the case)" do
     let(:employee) { nil }
 
     it "should not be able to read teams" do
