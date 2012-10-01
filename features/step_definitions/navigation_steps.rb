1d30876d72b1ad33d6e258a71609a1443db5ca86
Niklas Hofer <niklas+dev@lanpartei.de>
2012-10-01 17:31:00 +0200

    (cucumber) fix menu toggling

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,5 +1,5 @@
 When /^I choose "([^"]*)" from the drop down "([^"]*)"$/ do |item, dropdown|
-  #page.first('a.dropdown-toggle', :text => dropdown).click
+  step %~I open the "#{dropdown}" menu~
   step %~I follow "#{item}"~
 end
 
