004aae461e82295c6f910d2e31a8bf29a5ba3596
Niklas Hofer <niklas+dev@lanpartei.de>
2012-10-02 19:34:47 +0200

    Mail Matcher accept employees, users and mail addresses

--- spec/support/mail_matchers.rb
+++ spec/support/mail_matchers.rb
@@ -1,14 +1,6 @@
 RSpec::Matchers.define :have_received_mails do |count|
-  def all_mails(to=nil)
-    if to.nil?
-      ActionMailer::Base.deliveries
-    else
-      ActionMailer::Base.deliveries.select { |mail| mail.to == [to] }
-    end
-  end
   match do |address|
-    address = address.user if address.respond_to?(:user) # can throw an employee in here
-    address = address.email if address.respond_to?(:email) # can throw a user in here
+    address = resolve_email_address address
     mails = all_mails(address)
     if @subject.present?
       mails = mails.select {|m| m.subject == @subject }
@@ -28,6 +20,7 @@ RSpec::Matchers.define :have_received_mails do |count|
   end
 
   failure_message_for_should do |address|
+    address = resolve_email_address address
     "#{address} should have received #{count} mails, but received #{@count}".tap do |m|
       if @subject.present?
         m << " with subject #{@subject.inspect}"
@@ -38,21 +31,36 @@ RSpec::Matchers.define :have_received_mails do |count|
       m << "\nreceived_mails:\n#{all_mails(address).map {|m| dump_mail(m) }.join("\n\n")}"
     end
   end
-
-  def dump_mail(mail)
-    "#{mail.from.inspect} => #{mail.to.inspect} `#{mail.subject}`\n#{mail.body}"
-  end
 end
 
 RSpec::Matchers.define :have_received_no_mail do
   match do |address|
-    ActionMailer::Base.deliveries.select { |mail| mail.to == [address] }.empty?
+    address = resolve_email_address address
+    ActionMailer::Base.deliveries.none? { |mail| mail.to.include?(address) }
   end
 end
 
 module NotificationMatcher
   def have_been_notified(*a)
     have_received_mails(1)
+  end
+
+  def all_mails(to=nil)
+    if to.nil?
+      ActionMailer::Base.deliveries
+    else
+      ActionMailer::Base.deliveries.select { |mail| mail.to.include?(to) }
+    end
+  end
+
+  def dump_mail(mail)
+    "#{mail.from.inspect} => #{mail.to.inspect} `#{mail.subject}`\n#{mail.body}"
+  end
+
+  def resolve_email_address(address)
+    address = address.user if address.respond_to?(:user) # can throw an employee in here
+    address = address.email if address.respond_to?(:email) # can throw a user in here
+    address
   end
 end
 
