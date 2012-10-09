c65060f818638a33b8e13fc7c0e059ff7342d284
Niklas Hofer <niklas+dev@lanpartei.de>
2012-10-09 06:29:03 +0200

    no redirect loops on denied access

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -18,7 +18,7 @@ class WelcomeController < ApplicationController
   end
 
   def redirect_to_dynamic_dashboard_if_signed_in
-    if user_signed_in?
+    if user_signed_in? and not flash[:alert] # no redirect loop on access denied
       flash.keep
       redirect_to dynamic_dashboard_path
     end
