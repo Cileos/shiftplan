507bfc08052a7f367128098aba183b9f9e6c3d31
Niklas Hofer <niklas+dev@lanpartei.de>
2012-10-25 18:32:43 +0200

    handle multiple classes in render_cell_for_day

--- app/decorators/scheduling_filter_decorator.rb
+++ app/decorators/scheduling_filter_decorator.rb
@@ -75,7 +75,7 @@ class SchedulingFilterDecorator < ApplicationDecorator
   def render_cell_for_day(day, *a)
     cell_html_options = { data: cell_metadata(day, *a) }
     if outside_plan_period?(day)
-      cell_html_options.merge!(class: 'outside_plan_period')
+      cell_html_options[:class] = "outside_plan_period #{cell_html_options[:class]}"
     end
 
     h.content_tag :td, cell_content(day, *a), cell_html_options
