fb63036ba4c31136c93f36a4f3be02cccd012b4f
Raphaela Wrede <rw@cileos.com>
2012-11-29 14:18:23 +0100

    add spec for uniqueness of email within account and remove related scenarios instead

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -17,4 +17,49 @@ describe Invitation do
       user.reload.employees.should include(employee)
     end
   end
+
+  context 'uniqueness of email within the account' do
+      let(:homer)          { create :employee }
+      let(:bart)           { create :employee }
+      let(:account_1)      { create :account }
+      let(:organization_1) { create :organization, account: account_1 }
+      let(:email)          { 'thesimpsons@springfield.com' }
+
+      let!(:invitation_for_homer) do
+        create(:invitation,
+          organization: organization_1,
+          employee:     homer,
+          email:        email
+        )
+      end
+
+      let(:invitation_for_bart) do
+        build(:invitation,
+          organization: organization_2,
+          employee:     bart,
+          email:        email
+        )
+      end
+
+    context 'when email exists within same account' do
+      # organization within same account
+      let(:organization_2) { create :organization, account: account_1 }
+
+      it 'should not be valid' do
+        invitation_for_bart.should_not be_valid
+        invitation_for_bart.should have(1).errors_on(:email)
+      end
+    end
+
+    context 'when email exists within different account' do
+      let(:account_2)      { create :account }
+      # organization of a different account
+      let(:organization_2) { create :organization, account: account_2 }
+
+      it 'should be valid' do
+        invitation_for_bart.should be_valid
+      end
+    end
+
+  end
 end
