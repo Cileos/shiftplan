5fbc2959989cd2e86d5bd19147e57cf346f1c650
Raphaela Wrede <rw@cileos.com>
2012-11-29 13:24:11 +0100

    add account_id to invitations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120914152445) do
+ActiveRecord::Schema.define(:version => 20121129120135) do
 
   create_table "accounts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -81,8 +81,10 @@ ActiveRecord::Schema.define(:version => 20120914152445) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "email"
+    t.integer  "account_id"
   end
 
+  add_index "invitations", ["account_id"], :name => "index_invitations_on_account_id"
   add_index "invitations", ["email"], :name => "index_invitations_on_email"
   add_index "invitations", ["employee_id"], :name => "index_invitations_on_employee_id"
   add_index "invitations", ["inviter_id"], :name => "index_invitations_on_inviter_id"
