2a836ad3409cfa8dc3cb163f10d13f5d9180fd35
Raphaela Wrede <rw@cileos.com>
2012-12-04 14:53:14 +0100

    qualification has a unique name within organization

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -2,4 +2,5 @@ class Qualification < ActiveRecord::Base
   belongs_to :organization
 
   validates :name, :organization, presence: true
+  validates_uniqueness_of :name, scope: :organization_id
 end
