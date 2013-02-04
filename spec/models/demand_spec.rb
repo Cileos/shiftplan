70a36b904581283afb7ab670f27996337c6aa682
Raphaela Wrede <rw@cileos.com>
2013-02-04 21:25:20 +0100

    delete test case for demands shifts, we don't have them anymore

--- spec/models/demand_spec.rb
+++ spec/models/demand_spec.rb
@@ -8,13 +8,4 @@ describe Demand do
     build(:demand, quantity: 0).should_not be_valid
     build(:demand, quantity: -1).should_not be_valid
   end
-  it "destroys all its demands shifts when destroyed" do
-    demand = create(:demand)
-    demand.shifts << create(:shift)
-    demand.shifts << create(:shift)
-
-    lambda {
-      demand.destroy
-    }.should change(DemandsShifts, :count).from(2).to(0)
-  end
 end
