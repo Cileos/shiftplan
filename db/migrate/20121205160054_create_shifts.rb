ad250bf41c3d298a9d66d2005a2cc267be01b1b0
Raphaela Wrede <rw@cileos.com>
2013-02-05 11:13:27 +0100

    remove unnecessary migrations; add next_day_id to shifts when table shifts is created

--- db/migrate/20121205160054_create_shifts.rb
+++ db/migrate/20121205160054_create_shifts.rb
@@ -5,10 +5,12 @@ class CreateShifts < ActiveRecord::Migration
       t.datetime :starts_at
       t.datetime :ends_at
       t.integer  :team_id
+      t.integer  :next_day_id
 
       t.timestamps
     end
     add_index :shifts, :plan_template_id
     add_index :shifts, :team_id
+    add_index :shifts, :next_day_id
   end
 end
--- db/migrate/20130129124455_add_overnight_mate_id_to_shifts.rb
+++
@@ -1,6 +0,0 @@
-class AddOvernightMateIdToShifts < ActiveRecord::Migration
-  def change
-    add_column :shifts, :overnight_mate_id, :integer
-    add_index :shifts, :overnight_mate_id
-  end
-end
