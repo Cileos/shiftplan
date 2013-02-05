0c58741c9cbbb38627d951842e389dd32ec3ac2e
Raphaela Wrede <rw@cileos.com>
2013-02-05 10:26:14 +0100

    Improve comments describing the purpose of the TimeRangeComponentsAccessible module

--- app/models/time_range_components_accessible.rb
+++ app/models/time_range_components_accessible.rb
@@ -1,13 +1,19 @@
-# We want our planners only to have to give the date once and specify the start/end hour separately.
+# We want our planners only to have to give the date once and specify the start/end hour
+# separately. The user specifies time ranges using inputs for the time components
+# start_hour and end_hour. (start_minute and end_minute are optional, atm)
+# From these time components this module calculates starts_at and ends_at values that can
+# be persisted in the database.
+# It also rebuilds the time components from starts_at and ends_at for editing purposes.
 #
 # Prerequisites:
 #   @date is set to the wanted start date
 #   @start_hour, @end_hour are set
+#   @start_minute, @end_minute (optional atm)
 #   #end_hour_respecting_end_of_day
+#   #end_minute_respecting_end_of_day
 #
 # Result:
 #   #starts_at, #ends_at are set
-#   @next_day_end_hour is set if the ends_at would stretch to the next day
 #
 module TimeRangeComponentsAccessible
 
