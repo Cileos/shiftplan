5158d4cc2b66348959139f1de05de483d83da372
Niklas Hofer <niklas+dev@lanpartei.de>
2013-02-13 23:24:34 +0100

    (cucumber) can manipulate empty value again

--- features/step_definitions/form_manipulation_steps.rb
+++ features/step_definitions/form_manipulation_steps.rb
@@ -1,4 +1,4 @@
-When /^I manipulate the form #{capture_quoted} with attribute #{capture_quoted} and value #{capture_quoted}$/ do |form_class, attribute, value|
+When /^I manipulate the form #{capture_quoted} with attribute #{capture_quoted} and value #{capture_quoted_with_empty}$/ do |form_class, attribute, value|
   page.execute_script <<-EOJS
     $('form.#{form_class}').append('<input type="hidden" name="#{attribute}" value="#{value}" />')
   EOJS
