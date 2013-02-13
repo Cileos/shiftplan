325b1e826ddbde996a3799e74f61d4292a7ec908
Raphaela Wrede <rw@cileos.com>
2013-02-13 11:43:05 +0100

    extract edit url generation into superclass

--- app/decorators/record_decorator.rb
+++ app/decorators/record_decorator.rb
@@ -8,4 +8,21 @@ class RecordDecorator < ApplicationDecorator
     append_modal body: h.render('new_form', model_name => model),
       header: h.content_tag(:h3, heading)
   end
+
+  # Makes sure that always the first day of an overnightable is edited.
+  # The second day, if present, gets updated in after callbacks accordingly.
+  def edit_url
+    record_or_previous_day_record = if record.previous_day.present?
+      record.previous_day
+    else
+      record
+    end
+    h.url_for([:edit] + h.nested_resources_for(record_or_previous_day_record))
+  end
+
+  private
+
+    def record
+      model
+    end
 end
--- app/decorators/shift_decorator.rb
+++ app/decorators/shift_decorator.rb
@@ -11,15 +11,6 @@ class ShiftDecorator < RecordDecorator
     }
   end
 
-  # Makes sure that always the first day of an overnight shift is edited.
-  # The second day, if present, gets updated in after callbacks accordingly.
-  def edit_url
-    plan_template = shift.plan_template
-    organization = plan_template.organization
-    shift_or_previous_day_shift = shift.previous_day ? shift.previous_day : shift
-    h.url_for([:edit, organization.account, organization, plan_template, shift_or_previous_day_shift])
-  end
-
   def demands_sorted_by_qualification_name
     demands.sort_by { |d| d.try(:qualification).try(:name) || '' }
   end
