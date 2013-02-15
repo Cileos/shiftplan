71177f5eac18d90a5a04a230f4802b2647bf4988
Raphaela Wrede <rw@cileos.com>
2013-02-15 11:32:40 +0100

    remove self build locale support of action mailer as action mailer does support it in the format app/views/my_mailer/action_name.{locale}.html.haml already

--- config/initializers/localized_mails.rb
+++
@@ -1,7 +0,0 @@
-ActionMailer::Base.class_eval do
-
-  def mail_with_locale(headers={}, &block)
-    mail_without_locale(headers.merge(template_name: "#{action_name}_#{I18n.locale}", &block))
-  end
-  alias_method_chain :mail, :locale
-end
