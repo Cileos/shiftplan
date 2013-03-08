cf6af3b972dac02fa22de610ffbbc377f3128821
Raphaela Wrede <rw@cileos.com>
2013-03-08 13:04:16 +0100

    show teams not yet scheduled in the current week in a separate list in the legend

--- app/decorators/scheduling_filter_decorator.rb
+++ app/decorators/scheduling_filter_decorator.rb
@@ -141,12 +141,14 @@ class SchedulingFilterDecorator < ApplicationDecorator
     end
   end
 
-  def teams_of_records
-    records.map(&:team).compact.uniq.sort_by(&:name)
+  # active teams = teams already scheduled in current week
+  def active_teams
+    @active_teams ||= records.map(&:team).compact.uniq.sort_by(&:name)
   end
 
-  def teams
-    teams_of_records
+  # inactive teams = teams of organization not yet scheduled in current week
+  def inactive_teams
+    organization.teams.reject { |t| active_teams.include?(t) }
   end
 
   def hours_for(employee)
@@ -223,15 +225,17 @@ class SchedulingFilterDecorator < ApplicationDecorator
   end
 
   def legend
-    h.render('teams/legend', teams: teams)
+    h.render('teams/legend', active_teams: active_teams,
+             inactive_teams: inactive_teams)
   end
 
   def update_legend
     select(:legend).refresh_html legend
   end
 
+  # we only need to update colors of active teams
   def team_colors
-    h.render 'teams/colors', teams: teams
+    h.render 'teams/colors', teams: active_teams
   end
 
   def update_team_colors
