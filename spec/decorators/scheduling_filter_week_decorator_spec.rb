502b0302209c3b00183de8534210125cffdb3c7c
Niklas Hofer <niklas+dev@lanpartei.de>
2013-03-11 22:42:56 +0100

    do not check for calendar content in navigation feature

--- spec/decorators/scheduling_filter_week_decorator_spec.rb
+++ spec/decorators/scheduling_filter_week_decorator_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe SchedulingFilterWeekDecorator do
   let(:filter)    { SchedulingFilter.new }
   let(:decorator) { described_class.new filter }
+
+  it "contains only records in queried week"
+  it "does not contain records outside queried week"
+  it "groups records by week and y axis"
 end
