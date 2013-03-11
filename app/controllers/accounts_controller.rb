e5fb876d17b33b75b64456972abb9e0279bf6ce4
Raphaela Wrede <rw@cileos.com>
2013-03-11 12:22:51 +0100

    prepare new account (set virtual attars) in the controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,22 @@
 class AccountsController < InheritedResources::Base
   load_and_authorize_resource
+
   actions :all
 
-  after_filter :setup_account, :only => :create
+  after_filter  :setup_account,       only: :create
+  before_filter :prepare_new_account, only: :new
 
   protected
 
   def setup_account
     resource.setup
+  end
+
+  def prepare_new_account
+    resource.on_new_account = true
+    if current_user.employees.present?
+      resource.first_name = current_user.employees.first.first_name
+      resource.last_name = current_user.employees.first.last_name
+    end
   end
 end
