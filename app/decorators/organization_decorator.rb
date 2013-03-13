15ebb66dcca9de43e31c6371ebf347aa8b680ab5
Raphaela Wrede <rw@cileos.com>
2013-03-13 17:45:16 +0100

    update organisation menu items after creating or editing orgs

--- app/decorators/organization_decorator.rb
+++ app/decorators/organization_decorator.rb
@@ -1,2 +1,27 @@
 class OrganizationDecorator < TabularizedRecordDecorator
+
+  def respond
+    super
+    update_organization_menu_items if errors.empty?
+  end
+
+  private
+
+    def selector_for(name, record=nil, extra=nil)
+      case name
+      when :organization_menu
+        "ul.organization-dropdown"
+      else
+        super
+      end
+    end
+
+    def update_organization_menu_items
+      select(:organization_menu).refresh_html organization_menu_items
+    end
+
+    def organization_menu_items
+      h.render('organizations/menu_items',
+               organizations: h.current_account.organizations)
+    end
 end
