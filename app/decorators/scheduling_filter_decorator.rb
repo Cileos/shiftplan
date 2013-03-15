e4012e5f2c26e6044a4e5c171c9b158f7905b7c0
Niklas Hofer <niklas+dev@lanpartei.de>
2013-03-15 20:09:31 +0100

    move wwt_diff into SchedulingFilterEmployeesInWeekDecorator

--- app/decorators/scheduling_filter_decorator.rb
+++ app/decorators/scheduling_filter_decorator.rb
@@ -95,8 +95,6 @@ class SchedulingFilterDecorator < ApplicationDecorator
       end
     when :scheduling
       %Q~#calendar tbody .scheduling[data-edit_url="#{resource.decorate.edit_url}"]~
-    when :wwt_diff
-      %Q~#calendar tbody tr[data-employee-id=#{resource.id}] th .wwt_diff~
     when :legend
       '#legend'
     when :team_colors
@@ -113,33 +111,6 @@ class SchedulingFilterDecorator < ApplicationDecorator
      %Q~#calendar tbody td[data-date=#{scheduling.date.iso8601}][data-employee-id=#{scheduling.try(:employee_id) || 'missing'}]~
   end
 
-  def wwt_diff_for(employee)
-    h.abbr_tag(wwt_diff_label_text_for(employee, short: true),
-               wwt_diff_label_text_for(employee),
-               class: "badge #{wwt_diff_label_class_for(employee)}")
-  end
-
-  def wwt_diff_label_text_for(employee, opts={})
-    if employee.weekly_working_time.present?
-      opts[:short].present? ? txt = '/' : txt = 'of'
-      "#{hours_for(employee)} #{txt} #{employee.weekly_working_time.to_i}"
-    else
-      "#{hours_for(employee)}"
-    end
-  end
-
-  # the 'badge-normal' class is not actually used by bootstrap, but we cannot test for absent class
-  def wwt_diff_label_class_for(employee)
-    return 'badge-normal' unless employee.weekly_working_time.present?
-    difference = employee.weekly_working_time - hours_for(employee)
-    if difference > 0
-      'badge-warning'
-    elsif difference < 0
-      'badge-important'
-    else
-      'badge-success'
-    end
-  end
 
   def teams_of_records
     records.map(&:team).compact.uniq.sort_by(&:name)
@@ -205,16 +176,11 @@ class SchedulingFilterDecorator < ApplicationDecorator
   end
 
   def update_cell_for(scheduling)
-    update_wwt_diff_for(scheduling.employee) if scheduling.employee.present?
     select(:cell, scheduling).refresh_html cell_content(scheduling) || ''
   end
 
   def focus_element_for(scheduling)
     select(:scheduling, scheduling).trigger('focus')
-  end
-
-  def update_wwt_diff_for(employee)
-    select(:wwt_diff, employee).refresh_html wwt_diff_for(employee)
   end
 
 
