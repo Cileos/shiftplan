d9f97c07b6950b60003d93a0439861b194a0e086
Niklas Hofer <niklas+dev@lanpartei.de>
2013-04-26 22:09:24 +0200

    benchmark example cell view

--- spec/views/performance/teams_in_week_cell_performance_spec.rb
+++ spec/views/performance/teams_in_week_cell_performance_spec.rb
@@ -5,7 +5,7 @@ describe 'schedulings/lists/_teams_in_week.html.haml' do
   let(:plan) { create :plan }
   let(:n) { 10 }
 
-  let(:schedulings) {
+  let!(:schedulings) {
     (1..n).to_a.map do
       create :scheduling, plan: plan, team: create(:team), date: today, start_hour: 9, end_hour: 17
     end
@@ -14,23 +14,13 @@ describe 'schedulings/lists/_teams_in_week.html.haml' do
   let(:filter) { Scheduling.filter(cwyear: today.cwyear, week: today.cweek) }
   let(:preloaded) { filter.unsorted_records.all.map(&:decorate) }
 
-  before :each do
-    scheduling = schedulings.first
-    view.stub(:nested_resources_for).with(instance_of(Scheduling)).and_return( [
-      scheduling.organization.account,
-      scheduling.organization,
-      scheduling.plan,
-      scheduling
-    ] )
-
-    view.stub(:can? => true)
-  end
-
-  it "renders cell quickly" do
+  it "renders cell with 10 items in less than 30ms (average)", benchmark: true do # this is not really quick, is it?
     preloaded.should have(n).records
     expect do
-      render partial: 'schedulings/lists/teams_in_week', locals: { schedulings: preloaded }
-    end.to take_less_than(0.01)
+      10.times do
+        render partial: 'schedulings/lists/teams_in_week', locals: { schedulings: preloaded }
+      end
+    end.to take_less_than(0.3)
   end
 end
 
