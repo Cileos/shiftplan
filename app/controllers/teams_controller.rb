afb94371f1c39c23edc4e2ab08a44fe2ccb52c78
Raphaela Wrede <rw@cileos.com>
2013-04-30 15:40:19 +0200

    dry up views of tablelized resource team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,4 @@
-class TeamsController < InheritedResources::Base
+class TeamsController < TabularizedRecordsController
   belongs_to :account
   belongs_to :organization
   load_and_authorize_resource
