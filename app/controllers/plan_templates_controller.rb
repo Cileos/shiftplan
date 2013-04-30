a62c70a852dd2e794598aebe213371897bb72f49
Raphaela Wrede <rw@cileos.com>
2013-04-30 15:36:55 +0200

    dry up views of tablelized resource plan template

--- app/controllers/plan_templates_controller.rb
+++ app/controllers/plan_templates_controller.rb
@@ -1,4 +1,4 @@
-class PlanTemplatesController < InheritedResources::Base
+class PlanTemplatesController < TabularizedRecordsController
   belongs_to :account
   belongs_to :organization
   load_and_authorize_resource
