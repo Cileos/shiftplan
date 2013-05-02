e73d20224d907c22e7894f115022ccfbe1b865f6
Niklas Hofer <niklas+dev@lanpartei.de>
2013-05-02 20:01:06 +0200

    shorter schedulings should not be over-hovered by long ones
    fixes #882752

--- app/decorators/stack_decorator_helper.rb
+++ app/decorators/stack_decorator_helper.rb
@@ -1,13 +1,15 @@
 # helps stacking bars in hourly views
+# it is alchemy code: written live with the designer, untested and works just by magic.
+# TODO test
 module StackDecoratorHelper
   # Given an Array of Schedulings, it will arrange them in a way that no 2
-  # schedulings overlapping in their runtime are on the same layer,
+  # schedulings overlapping in their runtime are on the same stack,
   def pack_in_stacks(records)
     records.each { |r| r.stack = 0 }
-    records.sort_by!(&:length_in_hours)
+    records.sort_by! { |r| '%02d-%02d' % [ r.start_hour, 24 - r.length_in_hours] }
     [].tap do |stacked|
-      records.reverse.each do |current|
-        until stacked.select { |s| s.overlap?(current) }.empty?
+      records.each do |current|
+        until stacked.none? { |s| s.overlap?(current) }
           current.stack += 1
         end
         stacked << current
@@ -28,3 +30,7 @@ module StackDecoratorHelper
     }
   end
 end
+
+# later Schedulings are in righter stack than earliers
+# shorter Schedulings are in righter stack than long ones (so hovers of longs do not cover the shorts)
+# no Schedulings in same stack may overlap
