68c1678977a2d46dc95091e2bcb3da8883cb91dd
Raphaela Wrede <rw@cileos.com>
2013-05-03 10:49:25 +0200

    also destroy notifications for blog post when post is destroy
    - fixes broken dashboard
    - fixes #955228

--- spec/models/notification/post_spec.rb
+++ spec/models/notification/post_spec.rb
@@ -3,4 +3,18 @@ require 'spec_helper'
 describe Notification::Post do
   let(:notification) { described_class.new notifiable: create(:post) }
   it_should_behave_like 'Notification for Dashboard'
+
+  context "destroyed with its post" do
+    let(:post) { create :post }
+    let(:employee) { create :employee }
+    let(:notification) { described_class.new notifiable: post, employee: employee }
+
+    it 'is destroyed with its post' do
+      notification.save!
+      expect do
+        post.destroy
+      end.to change { Notification::Post.count }.from(1).to(0)
+    end
+  end
 end
+
