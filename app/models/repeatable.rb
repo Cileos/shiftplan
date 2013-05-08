52f8c32aba3e05ba5cb301bac0be3985b36fcd05
Raphaela Wrede <rw@cileos.com>
2013-05-08 15:39:03 +0200

    remove the repeater class; instead move all code needed to the repeatable module

--- app/models/repeatable.rb
+++ app/models/repeatable.rb
@@ -4,26 +4,44 @@ module Repeatable
     model.class_eval do
       after_save :create_repetitions
 
-      attr_accessor :repeat_for_days, :repeater
+      attr_accessor :repetitions
+
+      attr_accessor :repeat_for_days # TODO: :repeat_for_employees, :repeat_for_teams
     end
   end
 
   def create_repetitions
-    if repeat_for_days?
-      @repeater = Repeater.new(self)
-      @repeater.repeat!
+    repeat!
+  end
+
+  def repeat!
+    self.repetitions = []
+    repeat_for_days! if repeat_for_days.present?
+    # TODO
+    # repeat_for_employees!
+    # repeat_for_teams!
+  end
+
+  def repeat_for_days!
+    repeat_for_days.each do |day|
+      unless day.blank? || date.to_s == day
+        repetition = self.class.new(repeatable_attributes)
+        repetition.date = day
+        repetition.start_hour = start_hour
+        # Repetitions must be initialized with the original end hour of the
+        # repeatable so that they will become overnightables, too.
+        repetition.end_hour = is_overnight? ? next_day.end_hour : end_hour
+        repetition.save!
+        self.repetitions << repetition
+      end
     end
   end
 
-  def repeat_for_days?
-    repeat_for_days.present?
+  def non_repeatable_attributes
+    ['starts_at', 'ends_at', 'created_at', 'updated_at', 'next_day_id']
   end
 
-  def repetitions
-    if @repeater
-      @repeater.repetitions
-    else
-      []
-    end
+  def repeatable_attributes
+    attributes.except(*non_repeatable_attributes)
   end
 end
