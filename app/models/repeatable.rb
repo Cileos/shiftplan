480fbd4025003527e370d3cd34830d39289b4e32
Raphaela Wrede <rw@cileos.com>
2013-05-13 16:19:31 +0200

    Make Scheduling#repetitions default to empty array

--- app/models/repeatable.rb
+++ app/models/repeatable.rb
@@ -4,10 +4,14 @@ module Repeatable
     model.class_eval do
       after_save :create_repetitions
 
-      attr_accessor :repetitions
+      attr_writer :repetitions
 
       attr_accessor :repeat_for_days # TODO: :repeat_for_employees, :repeat_for_teams
     end
+  end
+
+  def repetitions
+    @repetitions || []
   end
 
   def create_repetitions
@@ -15,7 +19,6 @@ module Repeatable
   end
 
   def repeat!
-    self.repetitions = []
     repeat_for_days! if repeat_for_days.present?
     # TODO
     # repeat_for_employees!
@@ -23,7 +26,7 @@ module Repeatable
   end
 
   def repeat_for_days!
-    repeat_for_days.reject {|day| day.blank? || date == day.to_date }.each do |day|
+    self.repetitions = repeat_for_days.reject {|day| day.blank? || date == day.to_date }.map do |day|
       repetition = self.class.new(repeatable_attributes)
       repetition.date = day
       repetition.start_hour = start_hour
@@ -31,7 +34,7 @@ module Repeatable
       # repeatable so that they will become overnightables, too.
       repetition.end_hour = is_overnight? ? next_day.end_hour : end_hour
       repetition.save!
-      self.repetitions << repetition
+      repetition
     end
   end
 
