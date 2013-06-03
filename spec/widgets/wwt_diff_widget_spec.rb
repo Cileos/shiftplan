24743bf67500464316c0d4c18ee3de521cc9aa90
Niklas Hofer <niklas+dev@lanpartei.de>
2013-06-03 22:53:15 +0200

    move specs for WwtDiff away from using Decorator

--- spec/widgets/wwt_diff_widget_spec.rb
+++ spec/widgets/wwt_diff_widget_spec.rb
@@ -11,6 +11,7 @@ describe WwtDiffWidget do
     long  = stub 'long'
     subject.stub(:label_text).with(short: true).and_return(short)
     subject.stub(:label_text).with().and_return(long)
+    # color
     subject.stub(:label_class).and_return('wwt_class')
     view.should_receive(:abbr_tag).with(short, long, class: "badge wwt_class").and_return('tag')
     subject.to_html.should == 'tag'
@@ -45,6 +46,52 @@ describe WwtDiffWidget do
       Volksplaner::Formatter.stub(:human_hours).with(4.5).and_return(formatted)
       subject.stub hours: 4.5
       subject.human_hours.should == formatted
+    end
+  end
+
+  context 'color' do
+    let(:label_class) { subject.label_class }
+
+    it "is grey for employee without wwt" do
+      employee.stub weekly_working_time: nil
+      label_class.should == 'badge-normal'
+    end
+
+    it "is yellow for underscheduled employee" do
+      employee.stub weekly_working_time: 20
+      subject.stub hours: 10
+      label_class.should == 'badge-warning'
+    end
+
+    it "is green for exactly scheduled employee" do
+      employee.stub weekly_working_time: 20
+      subject.stub hours: 20
+      label_class.should == 'badge-success'
+    end
+
+    it "is read for overscheduled employee" do
+      employee.stub weekly_working_time: 20
+      subject.stub hours: 30
+      label_class.should == 'badge-important'
+    end
+  end
+
+  context 'label' do
+    let(:long_label) { subject.label_text }
+    let(:short_label) { subject.label_text(short: true) }
+
+    it "shows hours only for employee without wwt" do
+      employee.stub weekly_working_time: nil
+      subject.stub hours: 10
+      short_label.should == '10'
+      long_label.should == '10'
+    end
+
+    it "shows difference for employee with wwt" do
+      employee.stub weekly_working_time: 20
+      subject.stub hours: 10
+      short_label.should == '10 / 20'
+      long_label.should == '10 of 20'
     end
   end
 end
