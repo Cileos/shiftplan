3d97b574c2593cea54cc736b37f6ae0334d81401
Raphaela Wrede <rw@cileos.com>
2013-07-23 16:31:18 +0200

    start rewriting current specs; refactor currents module

--- lib/volksplaner/currents.rb
+++ lib/volksplaner/currents.rb
@@ -18,23 +18,12 @@ module Volksplaner::Currents
     end
   end
 
-  ######################################################################
-  # Account
-  ######################################################################
+  def prefetch_current_employee
+    current_employee || true
+  end
 
-  def find_current_account
-    if user_signed_in?
-      possibilities = current_user.accounts
-      if params[:account_id]
-        possibilities.find(params[:account_id])
-      elsif params[:controller] == 'accounts' && params[:id]
-        possibilities.find(params[:id])
-      elsif possibilities.count == 1
-        possibilities.first
-      end
-    end
-  rescue ActiveRecord::RecordNotFound => e
-    return nil
+  def prefetch_current_membership
+    current_membership || true
   end
 
   def current_account
@@ -45,41 +34,12 @@ module Volksplaner::Currents
     current_account.present?
   end
 
-  ######################################################################
-  # Organization
-  ######################################################################
-
-  def find_current_organization
-    if current_account?
-      possibilities = current_user.organizations_for(current_account)
-      if params[:organization_id]
-        possibilities.find(params[:organization_id])
-      elsif params[:controller] == 'organizations' && params[:id]
-        possibilities.find(params[:id])
-      elsif possibilities.count == 1
-        possibilities.first
-      end
-    end
-  end
-
   def current_organization
     @current_organization ||= find_current_organization
   end
 
   def current_organization?
     current_organization.present?
-  end
-
-
-  ######################################################################
-  # Employee
-  ######################################################################
-
-  # TODO load dynamically
-  def find_current_employee
-    if current_account?
-      current_user.current_employee = current_user.employee_for_account(current_account)
-    end
   end
 
   def current_employee
@@ -91,10 +51,6 @@ module Volksplaner::Currents
 
   def current_employee?
     current_employee.present?
-  end
-
-  def prefetch_current_employee
-    current_employee || true
   end
 
   ######################################################################
@@ -111,4 +67,72 @@ module Volksplaner::Currents
       nil
     end
   end
+
+  private
+
+  def find_current_account
+    if user_signed_in?
+      possibilities = current_user.accounts
+      if params[:controller] == 'accounts' && params[:id]
+        possibilities.find(params[:id])
+      elsif params[:account_id]
+        possibilities.find(params[:account_id])
+      elsif possibilities.count == 1
+        possibilities.first
+      end
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    return nil
+  end
+
+  def find_current_organization
+    if current_unambigous_organization.present?
+      current_unambigous_organization
+    elsif possible_organizations? && possible_organizations.count == 1
+      possible_organizations.first
+    end
+  end
+
+  def find_current_unambigous_organization
+    if current_account?
+      if params[:organization_id]
+        possible_organizations.find(params[:organization_id])
+      elsif params[:controller] == 'organizations' && params[:id]
+        possible_organizations.find(params[:id])
+      end
+    end
+  end
+
+  def find_current_membership
+    if current_unambigous_organization.present?
+      current_employee.memberships.find_by_organization_id(current_unambigous_organization.id)
+    end
+  end
+
+  def possible_organizations
+    if current_account?
+      @possible_organizations ||= current_user.organizations_for(current_account)
+    end
+  end
+
+  def possible_organizations?
+    possible_organizations.present?
+  end
+
+  def current_unambigous_organization
+    @current_unambigous_organization ||= find_current_unambigous_organization
+  end
+
+  def current_membership
+    return @current_membership if defined?(@current_membership)
+    current_user.current_membership = @current_membership = find_current_membership
+  end
+
+  # TODO load dynamically
+  def find_current_employee
+    if current_account?
+      current_user.current_employee = current_user.employee_for_account(current_account)
+    end
+  end
+
 end
