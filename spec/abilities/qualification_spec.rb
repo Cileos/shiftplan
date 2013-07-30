b4090be180252de829655d7f958cc0312ca45d08
Raphaela Wrede <rw@cileos.com>
2013-07-30 14:06:54 +0200

    adjust more availability specs to reflect new data model

--- spec/abilities/qualification_spec.rb
+++ spec/abilities/qualification_spec.rb
@@ -29,11 +29,12 @@ shared_examples "an employee who can manage qualifications" do
   end
 end
 
-shared_examples "an employee who can not read qualifications" do
+shared_examples "an employee who can not manage qualifications" do
   context "for own accounts" do
-    it "should not be able to read qualifications" do
-      should_not be_able_to(:read, create(:qualification, account: account))
-    end
+    it { should_not be_able_to(:read, build(:qualification, account: account)) }
+    it { should_not be_able_to(:create, build(:qualification, account: account)) }
+    it { should_not be_able_to(:update, build(:qualification, account: account)) }
+    it { should_not be_able_to(:destroy, build(:qualification, account: account)) }
   end
   context "for other accounts" do
     it_behaves_like "an employee who can not create, read, update and destroy qualifications" do
@@ -45,10 +46,11 @@ shared_examples "an employee who can not read qualifications" do
 end
 
 describe "Qualification permissions:" do
-  subject { ability }
-  let(:ability) { Ability.new(user) }
-  let(:user) { create(:user) }
-  let(:account) { create(:account) }
+  subject             {  ability }
+  let(:ability)       {  Ability.new(user) }
+  let(:user)          {  create(:user) }
+  let(:account)       {  create(:account) }
+  let(:organization)  {  create(:organization, account: account) }
 
   before(:each) do
     # simulate before_filter :set_current_employee
@@ -62,43 +64,33 @@ describe "Qualification permissions:" do
   end
 
   context "A planner" do
+    before(:each) do
+      # The planner role is set on the membership, so a planner can only be
+      # a planner for a certain membership/organization.
+      # Simulate CanCan's current_ability method by setting the current
+      # membership manually here.
+      user.current_membership = membership
+    end
+
     it_behaves_like "an employee who can manage qualifications" do
-      let(:employee) { create(:employee_planner, account: account, user: user) }
+      let(:employee) { create(:employee, account: account, user: user) }
+      let(:membership) do
+        create(:membership,
+          role: 'planner',
+          employee: employee,
+          organization: organization)
+      end
     end
   end
 
   context "An employee" do
-    before(:each) do
-      membership
-    end
-
-    it_behaves_like "an employee who can not read qualifications" do
+    it_behaves_like "an employee who can not manage qualifications" do
       let(:employee)     { create(:employee, account: account, user: user) }
       # An "normal" employee needs a membership for an organization to do things.
       # This is different from planners or owners which do not need a membership but
       # just the role "planner" or "owner" and belong to the acccount.
       let(:organization) { create(:organization, account: account) }
-      let(:membership)   { create(:membership, employee: employee, organization: organization) }
-    end
-  end
-
-  context "An user without employee(not possible but for the case)" do
-    let(:employee) { nil }
-
-    it "should not be able to read qualifications" do
-      should_not be_able_to(:read, create(:qualification, account: account))
-    end
-
-    it "should not be able to destroy qualifications" do
-      should_not be_able_to(:destroy, create(:qualification, account: account))
-    end
-
-    it "should not be able to create qualifications" do
-      should_not be_able_to(:create, create(:qualification, account: account))
-    end
-
-    it "should not be able to update qualifications" do
-      should_not be_able_to(:update, create(:qualification, account: account))
+      let!(:membership)   { create(:membership, employee: employee, organization: organization) }
     end
   end
 end
