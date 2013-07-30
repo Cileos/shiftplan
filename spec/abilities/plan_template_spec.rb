19159936edd649e45dda4417939461f3dbd4b55d
Raphaela Wrede <rw@cileos.com>
2013-07-30 19:20:14 +0200

    make plan template ability spec be alike plan ability spec

--- spec/abilities/plan_template_spec.rb
+++ spec/abilities/plan_template_spec.rb
@@ -1,64 +1,63 @@
 require 'spec_helper'
 require "cancan/matchers"
 
-shared_examples  "an employee who can not create, read, update and destroy plan_templates" do
-  it "should not be able to create plan_templates" do
-    should_not be_able_to(:create, create(:plan_template, organization: other_organization))
-  end
-  it "should not be able to read plan_templates" do
-    should_not be_able_to(:read, create(:plan_template, organization: other_organization))
-  end
-  it "should not be able to update plan_templates" do
-    should_not be_able_to(:update, create(:plan_template, organization: other_organization))
-  end
-  it "should not be able to destroy plan_templates" do
-    should_not be_able_to(:destroy, create(:plan_template, organization: other_organization))
-  end
-end
+describe "Plan template permissions:" do
+  shared_examples  "an employee with plan_template permissions for foreign accounts" do
+    let(:foreign_organization) { create(:organization) }
+    let(:foreign_plan_template)         { build(:plan_template, organization: foreign_organization) }
 
-shared_examples "an employee who can manage plan_templates" do
-  context "for own accounts" do
-    it "should be able to manage plan_templates" do
-      should be_able_to(:manage, create(:plan_template, organization: organization))
+    it "should not be able to CRUD plan_templates" do
+      should_not be_able_to(:create,  foreign_plan_template)
+      should_not be_able_to(:read,    foreign_plan_template)
+      should_not be_able_to(:update,  foreign_plan_template)
+      should_not be_able_to(:destroy, foreign_plan_template)
     end
   end
-  context "for other accounts" do
-    it_behaves_like "an employee who can not create, read, update and destroy plan_templates" do
-      let(:other_account) { create(:account) }
-      let(:other_organization) { create(:organization, account: other_account) }
-      let(:other_plan) { create(:plan, organization: other_organization) }
+
+  shared_examples "an employee with plan_template permissions without a membership" do
+    let(:another_organization) { create(:organization, account: account) }
+    let(:another_plan_template)         { build(:plan_template, organization: another_organization) }
+
+    it "should not be able to CRUD plan_templates" do
+      should_not be_able_to(:create,  another_plan_template)
+      should_not be_able_to(:read,    another_plan_template)
+      should_not be_able_to(:update,  another_plan_template)
+      should_not be_able_to(:destroy, another_plan_template)
     end
   end
-end
 
-describe "plan_template permissions:" do
   subject { ability }
-  let(:ability) { Ability.new(user) }
-  let(:user) { create(:user) }
-  let(:account) { create(:account) }
-  let(:organization) { create(:organization, account: account) }
+  let(:ability)       {  Ability.new(user) }
+  let(:user)          {  create(:user) }
+  let(:account)       {  create(:account) }
+  let(:organization)  {  create(:organization, account: account) }
+  let(:plan_template)          {  build(:plan_template, organization: organization) }
 
 
   before(:each) do
-    # simulate before_filter :set_current_employee
+    # The planner role is set on the membership, so a planner can only be
+    # a planner for a certain membership/organization.
+    # Simulate CanCan's current_ability method by setting the current
+    # membership and employee manually here.
+    user.current_membership = membership if membership
     user.current_employee = employee if employee
   end
 
   context "An owner" do
-    it_behaves_like "an employee who can manage plan_templates" do
-      let(:employee) { create(:employee_owner, account: account, user: user) }
+    let(:employee) { create(:employee_owner, account: account, user: user) }
+    let(:membership) { nil }
+
+    context "for own accounts" do
+      it "should be able to manage plan_templates" do
+        should be_able_to(:manage, plan_template)
+      end
+    end
+    context "for other accounts" do
+      it_behaves_like "an employee with plan_template permissions for foreign accounts"
     end
   end
 
   context "A planner" do
-    before(:each) do
-      # The planner role is set on the membership, so a planner can only be
-      # a planner for a certain membership/organization.
-      # Simulate CanCan's current_ability method by setting the current
-      # membership manually here.
-      user.current_membership = membership
-    end
-
     let(:employee) { create(:employee, account: account, user: user) }
     let(:membership) do
       create(:membership,
@@ -67,22 +66,18 @@ describe "plan_template permissions:" do
         organization: organization)
     end
 
-    it_behaves_like "an employee who can manage plan_templates"
+    context "for organizations with planner membership" do
+      it "should be able to manage plan_templates" do
+        should be_able_to(:manage, plan_template)
+      end
+    end
 
-    context "for other organizations" do
-      let(:another_organization) { create(:organization) }
-      it "should not be able to create plan_templates" do
-        should_not be_able_to(:create, create(:plan_template, organization: another_organization))
-      end
-      it "should not be able to read plan_templates" do
-        should_not be_able_to(:read, create(:plan_template, organization: another_organization))
-      end
-      it "should not be able to update plan_templates" do
-        should_not be_able_to(:update, create(:plan_template, organization: another_organization))
-      end
-      it "should not be able to destroy plan_templates" do
-        should_not be_able_to(:destroy, create(:plan_template, organization: another_organization))
-      end
+    context "for organizations without membership" do
+      it_behaves_like "an employee with plan_template permissions without a membership"
+    end
+
+    context "for other accounts" do
+      it_behaves_like "an employee with plan_template permissions for foreign accounts"
     end
   end
 
@@ -91,36 +86,23 @@ describe "plan_template permissions:" do
     # An "normal" employee needs a membership for an organization to do things.
     # This is different from planners or owners which do not need a membership but
     # just the role "planner" or "owner" and belong to the acccount.
-    let!(:membership) { create(:membership, employee: employee, organization: organization) }
+    let!(:membership)  {  create(:membership, employee: employee, organization: organization) }
 
-    it "should not be able to read plan_templates" do
-      should_not be_able_to(:read, create(:plan_template, organization: organization))
+    context "for organizations with membership" do
+      it "should not be able to CRUD plan_templates" do
+        should_not be_able_to(:create,  plan_template)
+        should_not be_able_to(:read,    plan_template)
+        should_not be_able_to(:update,  plan_template)
+        should_not be_able_to(:destroy, plan_template)
+      end
     end
-    it "should not be able to destroy plan_templates" do
-      should_not be_able_to(:destroy, create(:plan_template, organization: organization))
-    end
-    it "should not be able to create plan_templates" do
-      should_not be_able_to(:create, create(:plan_template, organization: organization))
-    end
-    it "should not be able to update plan_templates" do
-      should_not be_able_to(:update, create(:plan_template, organization: organization))
-    end
-  end
 
-  context "An user without employee(not possible but for the case)" do
-    let(:employee) { nil }
+    context "for organizations without membership" do
+      it_behaves_like "an employee with plan_template permissions without a membership"
+    end
 
-    it "should not be able to read plan_templates" do
-      should_not be_able_to(:read, create(:plan_template, organization: organization))
-    end
-    it "should not be able to destroy plan_templates" do
-      should_not be_able_to(:destroy, create(:plan_template, organization: organization))
-    end
-    it "should not be able to create plan_templates" do
-      should_not be_able_to(:create, create(:plan_template, organization: organization))
-    end
-    it "should not be able to update plan_templates" do
-      should_not be_able_to(:update, create(:plan_template, organization: organization))
+    context "for other accounts" do
+      it_behaves_like "an employee with plan_template permissions for foreign accounts"
     end
   end
 end
