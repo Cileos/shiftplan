c114bf7c1b38e705ffd3b44fbde3e8075f52febb
Raphaela Wrede <rw@cileos.com>
2013-08-30 16:20:46 +0200

    delete not used factory 'sent_notification'

--- factories/notifications.rb
+++ factories/notifications.rb
@@ -3,8 +3,4 @@ FactoryGirl.define do
   factory :notification, class: "Notification::Dummy" do
     employee
   end
-
-  factory :sent_notification, parent: :notification do
-    sequence(:sent_at) { 1.day.ago }
-  end
 end
