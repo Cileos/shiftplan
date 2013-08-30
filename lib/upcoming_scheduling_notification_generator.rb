5fa62e7d4e0f8960d0483cbe65931b99c174d78c
Raphaela Wrede <rw@cileos.com>
2013-08-30 12:41:52 +0200

    use by_notifiable scope in UpcomingSchedulingNotificationGenerator

--- lib/upcoming_scheduling_notification_generator.rb
+++ lib/upcoming_scheduling_notification_generator.rb
@@ -1,7 +1,7 @@
 class UpcomingSchedulingNotificationGenerator
   def self.generate!
     Scheduling.upcoming_in_the_next_24_hours.each do |scheduling|
-      last_notification = Notification::UpcomingScheduling.where(notifiable_id: scheduling.id, notifiable_type: 'Scheduling').order('created_at desc').first
+      last_notification = Notification::UpcomingScheduling.by_notifiable(scheduling).order('created_at desc').first
       if !last_notification.present? || last_notification.created_at < scheduling.updated_at
         Notification::UpcomingScheduling.create!(
           notifiable: scheduling,
