a80473273a8d80a7f669149b26561d208d578ba1
Niklas Hofer <niklas+dev@lanpartei.de>
2013-08-30 14:20:55 +0200

    do not change updated_at from GravatarUploader

--- app/services/gravatar_updater.rb
+++ app/services/gravatar_updater.rb
@@ -2,10 +2,12 @@ class GravatarUpdater
 
   # updates the avatar of the given record using Gravatar by the record#email
   def update(record, options={})
-    size = options.fetch(:size) { AvatarUploader::MaxSize }
-    url = record.gravatar_url secure: true, size: size, d: 404
-    record.remote_gravatar_url = url
-    record.save!
+    without_touching record do
+      size = options.fetch(:size) { AvatarUploader::MaxSize }
+      url = record.gravatar_url secure: true, size: size, d: 404
+      record.remote_gravatar_url = url
+      record.save!
+    end
   end
 
 
@@ -16,6 +18,17 @@ class GravatarUpdater
       unless record.avatar?
         update(record)
       end
+    end
+  end
+
+  private
+
+  # do not change #updated_at
+  def without_touching(record)
+    if record.class.respond_to?(:without_timestamps)
+      record.class.without_timestamps { yield }
+    else
+      yield
     end
   end
 
