10d5a7ceee14fae51286544983cda36b8577e354
Raphaela Wrede <rw@cileos.com>
2013-09-02 14:36:17 +0200

    extract method for setting the locale

--- app/mailers/upcoming_scheduling_notification_mailer.rb
+++ app/mailers/upcoming_scheduling_notification_mailer.rb
@@ -1,12 +1,16 @@
 class UpcomingSchedulingNotificationMailer < ClockworkMailer
 
   def upcoming_scheduling(notification)
-    user_locale = notification.employee.user.locale
-    I18n.locale = user_locale.present? ? user_locale.to_sym : I18n.default_locale
+    set_locale(notification)
     @notification = notification
     @scheduling = notification.notifiable
     mail to: notification.employee.user.email,
       subject: t(:'mailer.upcoming_scheduling_notification.upcoming_scheduling.subject')
+  end
+
+  def set_locale(notification)
+    user_locale = notification.employee.user.locale
+    I18n.locale = user_locale.present? ? user_locale.to_sym : I18n.default_locale
   end
 
 end
