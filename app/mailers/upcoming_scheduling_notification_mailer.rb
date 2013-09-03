6d93fc161c8274e4caf727951c5cf05e845339e4
Raphaela Wrede <rw@cileos.com>
2013-09-03 18:24:06 +0200

    include account, organization and plan name in the upcoming scheduling mail's subject

--- app/mailers/upcoming_scheduling_notification_mailer.rb
+++ app/mailers/upcoming_scheduling_notification_mailer.rb
@@ -4,8 +4,15 @@ class UpcomingSchedulingNotificationMailer < ClockworkMailer
     set_locale(notification)
     @notification = notification
     @scheduling = notification.notifiable
+    plan = @scheduling.plan
+    organization = plan.organization
+    account = organization.account
+
     mail to: notification.employee.user.email,
-      subject: t(:'mailer.upcoming_scheduling_notification.upcoming_scheduling.subject')
+      subject: t(:'mailer.upcoming_scheduling_notification.upcoming_scheduling.subject',
+                account: account.name,
+                organization: organization.name,
+                plan: plan.name)
   end
 
   def set_locale(notification)
--- spec/mailers/upcoming_scheduling_notification_mailer_spec.rb
+++ spec/mailers/upcoming_scheduling_notification_mailer_spec.rb
@@ -13,8 +13,8 @@ describe UpcomingSchedulingNotificationMailer do
   let(:scheduling) do
     create(:scheduling,
       starts_at: '2012-12-12 12:15', ends_at: '2012-12-12 18:30',
-      team: create(:team, organization: organization, name: 'Reaktor fegen'),
-      plan: create(:plan, organization: organization)
+      team: create(:team, organization: organization, name: 'Reactor sweeping'),
+      plan: create(:plan, organization: organization, name: 'Cleaning')
     )
   end
   let(:notification) do
@@ -41,11 +41,11 @@ describe UpcomingSchedulingNotificationMailer do
 
     context "when the user has no locale set" do
       it "has a german subject" do
-        mail.subject.should == 'Erinnerung: Anstehende Schicht'
+        mail.subject.should == 'Erinnerung: Anstehende Schicht (Springfield Nuclear Power Plant / Sector 7-G / Cleaning)'
       end
 
       it "has a german body" do
-        expect(mail.body).to include('Sie sind für eine Schicht am Mittwoch, 12.12.2012 (12:15-18:30 Reaktor fegen [Rf]) eingeteilt')
+        expect(mail.body).to include('Sie sind für eine Schicht am Mittwoch, 12.12.2012 (12:15-18:30 Reactor sweeping [Rs]) eingeteilt')
       end
     end
 
@@ -56,11 +56,11 @@ describe UpcomingSchedulingNotificationMailer do
       end
 
       it "has an english subject" do
-        mail.subject.should == 'Reminder: Upcoming shift'
+        mail.subject.should == 'Reminder: Upcoming shift (Springfield Nuclear Power Plant / Sector 7-G / Cleaning)'
       end
 
       it "has an english body" do
-        expect(mail.body).to include('You are scheduled for a shift on Wednesday, 12.12.2012 (12:15-18:30 Reaktor fegen [Rf]).')
+        expect(mail.body).to include('You are scheduled for a shift on Wednesday, 12.12.2012 (12:15-18:30 Reactor sweeping [Rs]).')
       end
     end
   end
