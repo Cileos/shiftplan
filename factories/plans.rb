96089797f11c9e6c5cf566abe3a6c128b381bd5b
Raphaela Wrede <rw@cileos.com>
2013-09-05 15:54:10 +0200

    use blocks in factory definition to make code evaluated when factory is used

--- factories/plans.rb
+++ factories/plans.rb
@@ -11,8 +11,8 @@ FactoryGirl.define do
     sequence(:ends_at) {|i| Time.zone.parse('1968-01-01') + (i + 2).hours }
 
     factory :upcoming_scheduling_in_the_next_24_hours do
-      starts_at Time.zone.now + 23.hours
-      ends_at Time.zone.now + 30.hours
+      starts_at { Time.zone.now + 23.hours }
+      ends_at   { Time.zone.now + 30.hours }
     end
   end
 
