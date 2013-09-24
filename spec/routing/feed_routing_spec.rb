f6f48c0b3a8f4e25550a0288ddac011f816665b4
Niklas Hofer <niklas+dev@lanpartei.de>
2013-09-24 20:03:48 +0200

    must escape email for the router to accept it as param

--- spec/routing/feed_routing_spec.rb
+++ spec/routing/feed_routing_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe 'routes for feeds' do
+  let(:token) { 'Aa' * 10 }
   it 'routes private URLs to feed controller' do
-    { get: '/feeds/foo@example.com/private-XYZ2342/upcoming' }.should route_to(
+    { get: "/feeds/foo%40example.com/private-#{token}/upcoming" }.should route_to(
       controller: 'feeds',
       action:     'upcoming',
       email:      'foo@example.com',
-      private_token: 'XYZ2342'
+      private_token: token
     )
+  end
+
+  # mitigate timing attacks
+  it 'ignores tokens longer than 20 chars' do
+    { get: "/feeds/foo%40example.com/private-#{'a' * 21}/upcoming" }.should_not be_routable
+  end
+
+  it 'ignores tokens shorter than 20 chars' do
+    { get: "/feeds/foo%40example.com/private-#{'a' * 19}/upcoming" }.should_not be_routable
   end
 end
