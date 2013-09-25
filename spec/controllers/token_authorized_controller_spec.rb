6b0edab166bab754d4166aba561a32c6aff58b64
Niklas Hofer <niklas+dev@lanpartei.de>
2013-09-25 18:44:36 +0200

    a wrong token should only cause a 404, not a 500

--- spec/controllers/token_authorized_controller_spec.rb
+++ spec/controllers/token_authorized_controller_spec.rb
@@ -2,10 +2,19 @@ describe TokenAuthorizedController do
   let(:private_token) { 'affe2342' }
   let(:email) { 'slave@example.com' }
   describe '#current_user' do
-    it 'finds user by email and private token' do
+    before :each do
       controller.stub params: { private_token: private_token, email: email }
+    end
+
+    it 'finds user by email and private token' do
       user = create :confirmed_user, private_token: private_token, email: email
       controller.send(:current_user).should == user
+    end
+
+    it 'induces 404 when user could not be found' do
+      expect {
+        controller.send(:current_user)
+      }.to raise_error(TokenAuthorizedController::UserNotFound)
     end
   end
 end
