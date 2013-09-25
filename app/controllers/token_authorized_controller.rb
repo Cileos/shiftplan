000342b4e60e1147ffcfdbb96ce4729039475976
Niklas Hofer <niklas+dev@lanpartei.de>
2013-09-25 18:44:36 +0200

    a wrong token should only cause a 404, not a 500

--- app/controllers/token_authorized_controller.rb
+++ app/controllers/token_authorized_controller.rb
@@ -7,9 +7,15 @@ class TokenAuthorizedController < ApplicationController
   skip_before_filter :authenticate_user!
   skip_authorization_check
 
+  class UserNotFound < ActiveRecord::RecordNotFound; end
+
+  rescue_from UserNotFound do
+    render status: 404, text: "Barbie said to Ken,\nthis is not the right token."
+  end
+
   private
 
   def current_user
-    @current_user ||= User.where(params.slice(:email, :private_token)).first
+    @current_user ||= User.where(params.slice(:email, :private_token)).first || raise(UserNotFound)
   end
 end
