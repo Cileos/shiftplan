8cf2e5851c4996699fce72a14fbd428d1a523894
Raphaela Wrede <rw@cileos.com>
2013-09-26 13:04:54 +0200

    spec no notification emails are sent when employee has no user

--- spec/models/notification/base_spec.rb
+++ spec/models/notification/base_spec.rb
@@ -2,6 +2,14 @@ describe Notification::Base do
 
   context "#deliver!" do
 
+    shared_examples :a_notification_not_sending_an_email do
+      it "does not deliver an email" do
+        mailer_class.should_receive(:my_mailer_action).never
+
+        notification.send(:deliver!)
+      end
+    end
+
     let(:notification)  { Notification::Base.new(employee: employee) }
     let(:employee)      { build(:employee, user: user) }
     let(:mailer_class)  { double('Mailer') }
@@ -11,19 +19,19 @@ describe Notification::Base do
       notification.class.stub(:mailer_action).and_return(:my_mailer_action)
     end
 
+    context "when the employee does not have a user" do
+      it_behaves_like :a_notification_not_sending_an_email do
+        let(:user) { nil }
+      end
+    end
+
     context "when the user has notification emails turned off" do
-
-      let(:user) { build(:user, receive_notification_emails: false) }
-
-      it "does not deliver an email" do
-        mailer_class.should_receive(:my_mailer_action).never
-
-        notification.send(:deliver!)
+      it_behaves_like :a_notification_not_sending_an_email do
+        let(:user) { build(:user, receive_notification_emails: false) }
       end
     end
 
     context "when the user has notification emails turned on" do
-
       let(:user) { build(:user, receive_notification_emails: true) }
 
       it "delivers an email" do
