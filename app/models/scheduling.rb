a99316de2e7e1101d895a2dfedf97ba0d39879eb
Niklas Hofer <niklas+dev@lanpartei.de>
2013-10-02 15:57:27 +0200

    CopyWeek should preserve minutes
    fixes #1131636

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -72,12 +72,16 @@ class Scheduling < ActiveRecord::Base
 
   # Because Date and Times are immutable, we have to situps to just change the week and year.
   # must be used on a valid record.
-  def move_to_week_and_year(week, year)
+  def move_to_week_and_year!(week, year)
     end_hour_or_end_hour_of_next_day = next_day ? next_day.end_hour : end_hour
-    *saved = start_hour, end_hour_or_end_hour_of_next_day
+    *saved_hours = start_hour, end_hour_or_end_hour_of_next_day
+    *saved_minutes = start_minute, end_minute
     @date = Date.commercial(year, week, cwday)
     self.next_day_id = self.starts_at = self.ends_at = self.week = self.year = nil
-    self.start_hour, self.end_hour = *saved
+    self.start_hour, self.end_hour = *saved_hours
+    self.start_minute, self.end_minute = *saved_minutes
+    save!
+    self
   end
 
   def date=(new_date)
--- spec/models/scheduling_spec.rb
+++ spec/models/scheduling_spec.rb
@@ -590,4 +590,35 @@ describe Scheduling do
       end.to change { scheduling.reload.comments_count }.from(1).to(0)
     end
   end
+
+  context '#move_to_week_and_year' do
+    let(:scheduling) { build :scheduling_by_quickie, quickie: '10:15-11:45' }
+    let(:moving) { lambda {
+      scheduling.move_to_week_and_year!(23, 2002)
+    } }
+
+    it 'changes year' do
+      expect(&moving).to change { scheduling.year }.to(2002)
+    end
+
+    it 'changes week' do
+      expect(&moving).to change { scheduling.week }.to(23)
+    end
+
+    it 'preserves start hour' do
+      expect(&moving).not_to change { scheduling.start_hour }
+    end
+
+    it 'preserves start minute' do
+      expect(&moving).not_to change { scheduling.start_minute }
+    end
+
+    it 'preserves end hour' do
+      expect(&moving).not_to change { scheduling.end_hour }
+    end
+
+    it 'preserves end minute' do
+      expect(&moving).not_to change { scheduling.end_minute }
+    end
+  end
 end
