2f1be7a9d416bb8ec820de658c3bb5728ec398b7
Niklas Hofer <niklas+dev@lanpartei.de>
2013-11-01 14:13:56 +0100

    Revert "move overnight-specific reponse into corresponding decorator"
    This reverts commit 7e67c32d3aa906fff6c73293574333c52640ba7a.
    
    The OvernightableDecoratorHelper is not used by the filters => old cell
    is not updated on scheduling edit.

--- app/decorators/responder_decorator_helper.rb
+++ app/decorators/responder_decorator_helper.rb
@@ -19,7 +19,12 @@ module ResponderDecoratorHelper
     end
   end
 
+  # OPTIMIZE extract Overnightable-specific code
   def respond_for_update(resource)
+    update_cell_for(resource.with_previous_changes_undone)
+    if resource.next_day
+      update_cell_for(resource.next_day.with_previous_changes_undone)
+    end
     respond_normally(resource)
   end
 
