aacdd05d67f87012237011554070bb7c287606e8
Raphaela Wrede <rw@cileos.com>
2013-11-05 18:13:07 +0100

    add spec for testing the total number of created post notifications

--- spec/models/notification/dispatcher/post_spec.rb
+++ spec/models/notification/dispatcher/post_spec.rb
@@ -1,6 +1,8 @@
 describe Notification::Dispatcher::Post do
   shared_examples :not_creating_a_notification_for_the_employee do
     it "does not create a notification" do
+      dispatcher.create_notifications!
+
       notifications.should be_empty
     end
   end
@@ -44,41 +46,51 @@ describe Notification::Dispatcher::Post do
 
   context "#create_notifications!" do
 
-    before(:each) do
-      the_employee # to create record beforehand
-      dispatcher.create_notifications!
-    end
-
     let(:notifications) do
       Notification::Post.where(employee_id: the_employee.id,
         notifiable_id: origin.id, notifiable_type: 'Post')
     end
 
+    it "creates the expected total number of notifications" do
+      coworker
+      author
+      unconfirmed_coworker
+      coworker_without_user
+      other_employee
+
+      expect do
+        dispatcher.create_notifications!
+      end.to change(Notification::Post, :count).from(0).to(1)
+    end
+
+
     context "for a confirmed coworker" do
-      let(:the_employee) { coworker }
+      let!(:the_employee) { coworker }
 
       it "creates a notification" do
+        dispatcher.create_notifications!
+
         notifications.should_not be_empty
       end
     end
 
     context "for the author of the post" do
-      let(:the_employee) { author }
+      let!(:the_employee) { author }
       it_behaves_like :not_creating_a_notification_for_the_employee
     end
 
     context "for an unconfirmed coworker" do
-      let(:the_employee) { unconfirmed_coworker }
+      let!(:the_employee) { unconfirmed_coworker }
       it_behaves_like :not_creating_a_notification_for_the_employee
     end
 
     context "for a coworker without a user" do
-      let(:the_employee) { coworker_without_user }
+      let!(:the_employee) { coworker_without_user }
       it_behaves_like :not_creating_a_notification_for_the_employee
     end
 
     context "for an employee of a different organization" do
-      let(:the_employee) { coworker_without_user }
+      let!(:the_employee) { other_employee }
       it_behaves_like :not_creating_a_notification_for_the_employee
     end
   end
