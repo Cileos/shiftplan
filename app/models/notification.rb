6ab33c0adf7821854d0f2c64e585309a4eb3b0cc
Raphaela Wrede <rw@cileos.com>
2013-11-11 16:16:29 +0100

    delete old and not used method Notification.create_for

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,4 @@
 module Notification
-  def self.create_for(thingy)
-    Notification::Dispatcher::Base.create_notifications_for(thingy)
-  end
-
   def self.destroy_for(thingy)
     Notification::Base.by_notifiable(thingy).each(&:destroy)
   end
