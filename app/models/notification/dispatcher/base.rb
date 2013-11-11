809df3023067a735d4c26523b878b63b4f4ac3d8
Raphaela Wrede <rw@cileos.com>
2013-11-11 11:50:20 +0100

    remove old notification dispatcher classes and their specs; they are not used anymore

--- app/models/notification/dispatcher/base.rb
+++
@@ -1,7 +0,0 @@
-class Notification::Dispatcher::Base
-  attr_reader :origin
-
-  def initialize(origin)
-    @origin = origin
-  end
-end
--- app/models/notification/dispatcher/comment.rb
+++
@@ -1,8 +0,0 @@
-class Notification::Dispatcher::Comment < Notification::Dispatcher::Base
-
-  private
-
-  def comment
-    origin
-  end
-end
