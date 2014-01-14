c43818c20e7c5ff4076589d2b8ac49c5f1c9f907
Raphaela Wrede <rw@cileos.com>
2014-01-14 14:15:45 +0100

    extract shared example to remove duplicated code

--- spec/models/notification_creator_spec.rb
+++ spec/models/notification_creator_spec.rb
@@ -5,33 +5,41 @@ describe NotificationCreator do
   let(:notifiable) { double('Notifiable') }
   let(:creator)    { described_class.new notifiable }
 
-  let(:recipients_finder_mock) { double('RecipientsFinder') }
-  let(:klass_finder_mock)      { double('KlassFinder') }
   let(:homer_simpson)          { double('Homer Simpson') }
   let(:bart_simpson)           { double('Bart Simpson') }
   let(:recipients)             { [homer_simpson, bart_simpson] }
   let(:klass_1)                { double('Some notification class') }
   let(:klass_2)                { double('Another notification class') }
 
+  shared_examples :each_notification_creator do
+    it "creates notifications of the right type for each recipient with expected finders" do
+      recipients_finder.stub(:[]).with(notifiable).and_return(recipients)
+
+      klass_finder.stub(:[]).with(notifiable, homer_simpson).and_return(klass_1)
+      klass_finder.stub(:[]).with(notifiable, bart_simpson).and_return(klass_2)
+
+      klass_1.should_receive(:create!).with(notifiable: notifiable, employee: homer_simpson)
+      klass_2.should_receive(:create!).with(notifiable: notifiable, employee: bart_simpson)
+
+      creator.create!
+    end
+  end
 
   context "#create!" do
 
     context "without options for recipients and klass finder" do
+      let(:recipients_finder_mock) { double('RecipientsFinder') }
+      let(:klass_finder_mock)      { double('KlassFinder') }
 
-      it "creates notifications of the right type for each recipient with default finders" do
+      before(:each) do
         Volksplaner.stub(:notification_recipients_finder => recipients_finder_mock)
-        recipients_finder_mock.stub(:[]).with(notifiable).and_return(recipients)
-
         Volksplaner.stub(:notification_klass_finder => klass_finder_mock)
-        klass_finder_mock.stub(:[]).with(notifiable, homer_simpson).and_return(klass_1)
-        klass_finder_mock.stub(:[]).with(notifiable, bart_simpson).and_return(klass_2)
-
-        klass_1.should_receive(:create!).with(notifiable: notifiable, employee: homer_simpson)
-        klass_2.should_receive(:create!).with(notifiable: notifiable, employee: bart_simpson)
-
-        creator.create!
       end
 
+      it_behaves_like :each_notification_creator do
+        let(:recipients_finder)  { recipients_finder_mock}
+        let(:klass_finder)       { klass_finder_mock}
+      end
     end
 
     context "with options for recipients and klass finder" do
@@ -45,18 +53,10 @@ describe NotificationCreator do
       let(:special_recipients_finder) { double('Special recipients finder') }
       let(:special_klass_finder)      { double('Special klass finder') }
 
-      it "creates notifications of the right type for each recipient with the specified finders" do
-        special_recipients_finder.stub(:[]).with(notifiable).and_return(recipients)
-
-        special_klass_finder.stub(:[]).with(notifiable, homer_simpson).and_return(klass_1)
-        special_klass_finder.stub(:[]).with(notifiable, bart_simpson).and_return(klass_2)
-
-        klass_1.should_receive(:create!).with(notifiable: notifiable, employee: homer_simpson)
-        klass_2.should_receive(:create!).with(notifiable: notifiable, employee: bart_simpson)
-
-        creator.create!
+      it_behaves_like :each_notification_creator do
+        let(:recipients_finder)  { special_recipients_finder}
+        let(:klass_finder)       { special_klass_finder }
       end
-
     end
 
   end
