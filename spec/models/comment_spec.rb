9c12c505dcc940741dd80645422a18c5c4e65d33
Raphaela Wrede <rw@cileos.com>
2014-01-14 12:16:17 +0100

    test creation of notifications for a created comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,18 +2,30 @@ require 'spec_helper'
 
 describe Comment do
 
+  let(:comment) do
+    Comment.build_from(create(:scheduling), create(:employee), body: 'Hallo')
+  end
+
   context "when destroyed" do
 
-    let(:comment) do
-      Comment.build_from(create(:scheduling), create(:employee), body: 'Hallo').tap(&:save!)
-    end
-
     it "notifications are destroyed" do
+      comment.save!
       destroyer = instance_double("NotificationDestroyer")
       NotificationDestroyer.should_receive(:new).with(comment).and_return(destroyer)
       destroyer.should_receive(:destroy!)
 
       comment.destroy
+    end
+  end
+
+  context "when created" do
+
+    it "notifications are created" do
+      creator = instance_double("NotificationCreator")
+      NotificationCreator.should_receive(:new).with(comment).and_return(creator)
+      creator.should_receive(:create!)
+
+      comment.save!
     end
   end
 end
