b0c79245caa75756f8bc699d6197078bd8bacbfd
Raphaela Wrede <rw@cileos.com>
2014-01-17 18:49:15 +0100

    do not require jQuery ui datepicker assets; we do not use the picker anymore

--- app/assets/javascripts/lib.js
+++ app/assets/javascripts/lib.js
@@ -21,7 +21,6 @@
 //= require fix_iOS_rotation
 //= require StickyTableHeaders
 //= require jquery.autosize
-//= require locales/jquery.ui.datepicker-de.js
 //= require jquery.cookie
 //= require jquery.mousewheel
 //= require jquery.timeentry
