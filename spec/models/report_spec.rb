9b798fc85107b1858c9aa268ae09955f147720c8
Raphaela Wrede <rw@cileos.com>
2014-03-26 09:45:46 +0100

    improve spec for filter by date

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -9,17 +9,18 @@ describe Report do
   let(:plan)           { create(:plan, organization: organization) }
 
   context "filter by date" do
-    let(:s1) { create(:scheduling, plan: plan, quickie: '0-24', date: '03.12.2012' ) }
-    let(:s2) { create(:scheduling, plan: plan, quickie: '1-2', date: '03.12.2012' ) }
-    let(:s3) { create(:scheduling, plan: plan, quickie: '14-18', date: '10.12.2012' ) }
+    let!(:s0) { create(:scheduling, plan: plan, quickie: '8-24',  date: '02.12.2012' ) }
+    let!(:s1) { create(:scheduling, plan: plan, quickie: '0-24',  date: '03.12.2012' ) }
+    let!(:s2) { create(:scheduling, plan: plan, quickie: '1-2',   date: '03.12.2012' ) }
+    let!(:s3) { create(:scheduling, plan: plan, quickie: '14-18', date: '10.12.2012' ) }
+    let!(:s4) { create(:scheduling, plan: plan, quickie: '0-8',   date: '11.12.2012' ) }
 
     let(:report_params) do
       super().merge(from: '03.12.2012', to: '10.12.2012')
     end
 
-    it "includes schedulings starting at the beginning and end of day" do
-      expected_records = [s3, s2, s1]
-      report.records.should == expected_records
+    it "includes schedulings in given range" do
+      report.records.should == [s3, s2, s1]
     end
   end
 
