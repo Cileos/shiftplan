ba08c5ffaf6600cde30819c1d0ea103f5a9161ac
Raphaela Wrede <rw@cileos.com>
2014-03-26 10:08:18 +0100

    add spec for filtering account or organization wide

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -8,6 +8,13 @@ describe Report do
   let(:organization)   { create(:organization, account: account) }
   let(:plan)           { create(:plan, organization: organization) }
 
+  let(:other_organization)  { create(:organization, account: account) }
+  let(:other_plan)          { create(:plan, organization: other_organization) }
+
+  before do
+    Timecop.freeze('15.10.2014')
+  end
+
   context "filter by date" do
     let!(:s0) { create(:scheduling, plan: plan, quickie: '8-24',  date: '02.12.2012' ) }
     let!(:s1) { create(:scheduling, plan: plan, quickie: '0-24',  date: '03.12.2012' ) }
@@ -21,6 +28,30 @@ describe Report do
 
     it "includes schedulings in given range" do
       report.records.should == [s3, s2, s1]
+    end
+  end
+
+  context "filter account or organization wide" do
+
+    let!(:s0) { create(:scheduling, plan: plan, quickie: '8-24',  date: '03.10.2014' ) }
+    let!(:s1) { create(:scheduling, plan: other_plan, quickie: '8-24',  date: '02.10.2014' ) }
+
+    context "when no organization given" do
+
+      it "finds schedulings within the account" do
+        report.records.should == [s0, s1]
+      end
+    end
+
+    context "when organization given" do
+
+      let(:report_params) do
+        super().merge(organization: organization)
+      end
+
+      it "finds schedulings within the organization" do
+        report.records.should == [s0]
+      end
     end
   end
 
