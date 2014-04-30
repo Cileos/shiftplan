c898fbb2825165f75ce1440328b10e079ead1f6b
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-04-30 11:49:59 +0200

    refactor report_helper

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,9 +1,13 @@
 module ReportsHelper
   def csv_export_params
-    { format: :csv, report: (params[:report] || {}).merge(limit: 'all') }
+    { format: :csv, report: params_report }
   end
 
   def xls_export_params
-    { format: :xls, report: (params[:report] || {}).merge(limit: 'all') }
+    { format: :xls, report: params_report }
+  end
+
+  def params_report
+    (params[:report] || {}).merge(limit: 'all')
   end
 end
