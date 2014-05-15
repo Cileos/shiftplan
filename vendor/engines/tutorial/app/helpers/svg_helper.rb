4f10f9bd8a724d050d1cb01bf1fec676ff801bf4
Niklas Hofer <niklas+dev@lanpartei.de>
2014-05-15 22:31:39 +0200

    replace path with component-handlebars

--- vendor/engines/tutorial/app/helpers/svg_helper.rb
+++ vendor/engines/tutorial/app/helpers/svg_helper.rb
@@ -2,6 +2,22 @@ module SvgHelper
   Root = Pathname.new(__FILE__).join('../../assets/images')
   def inline_svg(name, opts={})
     path = Root.join(name + '.svg')
-    File.read(path).html_safe
+    interactify( File.read(path) ).html_safe
+  end
+
+  def interactify(xml)
+    ret = xml
+    n = Nokogiri::XML(xml)
+    n.css('path[id^=tutorial_]').each do |elem|
+
+      attrs = elem.attributes.map do |k,a|
+        %Q~#{k}="#{a}"~
+      end.join(" ")
+      int = %Q~{{interactive-path #{attrs}}}~
+      elem.parent.add_child int
+
+      elem.remove
+    end
+    n.to_html
   end
 end
