763492a1234c6c0cc2784e8d11aa5cea8dafb8cc
Niklas Hofer <niklas+dev@lanpartei.de>
2014-05-15 17:50:45 +0200

    revise una permissions

--- spec/abilities/unavailability_ability_spec.rb
+++ spec/abilities/unavailability_ability_spec.rb
@@ -5,54 +5,29 @@ require "cancan/matchers"
 describe 'Unavailability permissions:' do
   subject              { ability }
   let(:ability)        { Ability.new(user) }
-  let(:user)           { create(:user) }
-  let(:other_user)     { create(:user) }
-  let(:account)        { create(:account) }
-  let(:organization)   { create(:organization, account: account) }
-  let(:other_account)  { create(:account) }
+  let(:user)           { build_stubbed(:user) }
   let(:employee)       { build(:employee) }
 
-  before :each do
-    user.stub accounts: [account]
-  end
+  describe 'for user' do
+    let(:unavailability) { build :unavailability, employee: employee }
 
-  describe "user being planner for the una's employee" do
-    before :each do
-      user.stub plannable_employees: [employee]
-    end
-
-    context "in one of the users accounts" do
-      let(:unavailability) { build :unavailability, accounts: [account], user: employee.user, employee: employee }
-      it 'can manage' do
-        should be_able_to(:manage, unavailability)
-      end
-    end
-
-    context "outside of the user's accounts" do
-      let(:unavailability) { build :unavailability, accounts: [other_account, account], user: employee.user, employee: employee }
-      it 'cannot manage' do
-        should_not be_able_to(:manage, unavailability)
-      end
-    end
-  end
-
-  describe 'self-planning user' do
     before :each do
       user.stub plannable_employees: []
+      user.stub employees: []
     end
 
-    context "in any of the users accounts" do
-      let(:unavailability) { build :unavailability, accounts: [account], user: user, employee: nil }
-      it 'can manage' do
-        should be_able_to(:manage, unavailability)
-      end
+    it 'allows me to manage unas for my plannable employees' do
+      user.plannable_employees << employee
+      should be_able_to(:manage, unavailability)
     end
 
-    context "outside of the user's accounts" do
-      let(:unavailability) { build :unavailability, accounts: [other_account, account], user: user, employee: nil }
-      it 'cannot manage' do
-        should_not be_able_to(:manage, unavailability)
-      end
+    it 'allows me to manage unas for my alter ego employees' do
+      user.employees << employee
+      should be_able_to(:manage, unavailability)
+    end
+
+    it 'forbids me to manage unas for any other employe' do
+      should_not be_able_to(:manage, unavailability)
     end
   end
 end
