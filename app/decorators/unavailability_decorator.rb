649639bd49fb2834b33ba9ee33d7f34f519449ca
Niklas Hofer <niklas+dev@lanpartei.de>
2014-05-16 17:00:20 +0200

    dont fail on unset reason

--- app/decorators/unavailability_decorator.rb
+++ app/decorators/unavailability_decorator.rb
@@ -2,6 +2,8 @@ class UnavailabilityDecorator < RecordDecorator
   include SchedulableDecoratorHelper
 
   def reason_text
-    I18n.t("activerecord.values.unavailability.reasons.#{reason}")
+    if reason
+      I18n.t("activerecord.values.unavailability.reasons.#{reason}")
+    end
   end
 end
