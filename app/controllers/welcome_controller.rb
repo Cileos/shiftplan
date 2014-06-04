98cb9f3643a68838293519bab50a13008414314c
Niklas Hofer <niklas+dev@lanpartei.de>
2014-06-04 12:41:33 +0200

    move Tutorial::Controller into engine

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,24 +1,3 @@
-module Tutorial::Controller
-
-  def self.included(controller)
-    controller.class_eval do
-      extend ClassMethods
-      attr_accessor :current_tutorial
-    end
-  end
-
-  module ClassMethods
-    # last one matching will be used
-    def tutorial(name, opts={})
-      before_filter opts do |ctrl|
-        ctrl.current_tutorial = name
-      end
-    end
-  end
-
-end
-
-
 class WelcomeController < ApplicationController
   include Tutorial::Controller
 
