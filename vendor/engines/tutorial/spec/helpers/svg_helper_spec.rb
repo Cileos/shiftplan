ae2731f568d89ccb2d5f587a71995ad808883ad8
Niklas Hofer <niklas+dev@lanpartei.de>
2014-06-06 18:14:57 +0200

    extract SvgInteractifier

--- vendor/engines/tutorial/spec/helpers/svg_helper_spec.rb
+++ vendor/engines/tutorial/spec/helpers/svg_helper_spec.rb
@@ -2,31 +2,12 @@ require 'spec_helper'
 
 describe SvgHelper, type: 'helper' do
   describe '#interactify' do
-    def svg(inner)
-      %Q~<svg inkscape:version="0.48"> #{inner} </svg>~
-    end
-
-    def interactify(x, *a)
-      helper.interactify( svg(x), *a )
-    end
-
-    it 'does not touch svg' do
-      interactify('  ').should have_selector('svg')
-    end
-
-    describe 'with paths matching our preference' do
-      let(:path_xml) { '<path id="tutorial_foo" d="long"></path>' }
-      let(:result) { interactify(path_xml) }
-
-      it 'adds handlebar component version of the wanted paths' do
-        result.should include(
-          %Q~{{interactive-path id="tutorial_foo" d="long"}}~
-        )
-      end
-
-      it 'removes the original element' do
-        result.should_not have_selector('svg path')
-      end
+    it 'runs SvgInteractifier#interactify' do
+      xml = double
+      interactifier = instance_double 'SvgInteractifier', interactify: (result = double)
+      SvgInteractifier.stub new: interactifier
+      helper.interactify(xml).should == result
     end
   end
+
 end
