6e6e743cca2edb446cba40a573bf129e4a526ffe
Niklas Hofer <niklas+dev@lanpartei.de>
2014-06-06 17:06:28 +0200

    add rough english translations for the tutorial

---
+++ vendor/engines/tutorial/spec/helpers/map_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'The tutorial map' do
+  let(:path)     { File.expand_path('../../../app/assets/images/map.svg', __FILE__) }
+  let(:svg)      { Nokogiri::XML( File.read(path) ) }
+  let(:markers)  { (svg / 'path[id^=tutorial_step]').map { |e| e[:id].scan(/^tutorial_step_(.*)$/) }.flatten }
+  let(:chapters) { I18n.translate('tutorial.chapters').map { |c| c['id'] } }
+
+  context 'markers' do
+    shared_examples :matching_markers_and_chapters do |locale|
+      in_locale locale
+      it "has a marker for every #{locale} chapter" do
+        expect(chapters- markers).to eq([])
+      end
+
+      it "has a #{locale} chapter for every marker" do
+        expect(markers - chapters).to eq([])
+      end
+    end
+
+    it_should_behave_like :matching_markers_and_chapters, 'de'
+    it_should_behave_like :matching_markers_and_chapters, 'en'
+  end
+end
