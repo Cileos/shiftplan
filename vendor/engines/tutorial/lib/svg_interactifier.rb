a789c485934661f49a24a6cd1e8ba39d4b475d90
Niklas Hofer <niklas+dev@lanpartei.de>
2014-06-10 17:08:03 +0200

    colorize done markers

--- vendor/engines/tutorial/lib/svg_interactifier.rb
+++ vendor/engines/tutorial/lib/svg_interactifier.rb
@@ -9,23 +9,34 @@ class SvgInteractifier
   def interactify(xml)
     ret = xml
     n = Nokogiri::XML(xml)
-    interactify_element(n, 'path[id^=tutorial_]', 'interactive-path')
+    interactify_element(n, 'path[id^=tutorial_]', 'interactive-path', bindings: { 'tutorial_step' => 'chapter' })
     n.to_html
   end
 
 private
-  def interactify_element(document, selector, component_name)
+  def interactify_element(document, selector, component_name, options={})
     document.css(selector).each do |elem|
 
       attrs = elem.attributes.map do |k,a|
         %Q~#{k}="#{a}"~
       end.join(" ")
       Rails.logger.debug { "attrs: #{elem.attributes.keys.join(' ')}" }
-      int = %Q~{{#{component_name} #{attrs}}}~
+      binds = binds_for(elem, options[:bindings])
+      int = %Q~{{#{component_name} #{attrs} #{binds} }}~
       elem.parent.add_child int
 
       elem.remove
     end
-
   end
+
+  def binds_for(element, binds)
+    return '' unless binds.present?
+
+    binds.map do |id_prefix, prefix|
+      signi = element['id'].sub(id_prefix + '_', '')
+
+      %Q~#{prefix}=#{prefix}_#{signi}~
+    end.join(' ')
+  end
+
 end
