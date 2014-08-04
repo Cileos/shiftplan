3a8e0306eb45fa02b2655e7c37d1631e3bf3c1b6
Niklas Hofer <niklas+dev@lanpartei.de>
2014-08-04 17:00:27 +0200

    migrations: s/uniq:/unique:/

--- db/migrate/20130802114446_add_slug_to_account.rb
+++ db/migrate/20130802114446_add_slug_to_account.rb
@@ -2,6 +2,6 @@ class AddSlugToAccount < ActiveRecord::Migration
   def change
     add_column :accounts, :slug, :string
     execute %Q~UPDATE accounts SET slug = id~
-    add_index :accounts, :slug, uniq: true
+    add_index :accounts, :slug, unique: true
   end
 end
--- db/migrate/20130802114504_add_slug_to_organization.rb
+++ db/migrate/20130802114504_add_slug_to_organization.rb
@@ -2,6 +2,6 @@ class AddSlugToOrganization < ActiveRecord::Migration
   def change
     add_column :organizations, :slug, :string
     execute %Q~UPDATE organizations SET slug = id~
-    add_index :organizations, :slug, uniq: true
+    add_index :organizations, :slug, unique: true
   end
 end
