7b48a3adc8c5019b7d7d142a45fffb1c705857e1
Niklas Hofer <niklas+dev@lanpartei.de>
2014-08-05 15:59:04 +0200

    trying to keep RJS alive

---
+++ config/initializers/_hacks.rb
@@ -0,0 +1,19 @@
+# Keep versatile_rjs alive
+#
+
+module ActiveSupport
+  module JSON
+    # Deprecated: A string that returns itself as its JSON-encoded form.
+    class Variable < String
+      def initialize(*args)
+        message = 'ActiveSupport::JSON::Variable is deprecated and was removed in Rails 4.1. ' \
+                  'We still need to for versatile_rjs to work'
+        ActiveSupport::Deprecation.warn message
+        super
+      end
+
+      def as_json(options = nil) self end #:nodoc:
+      def encode_json(encoder) self end #:nodoc:
+    end
+  end
+end
