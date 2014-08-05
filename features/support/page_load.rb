0b6444aed4358c330ffb091374bc197daa7a97a3
Niklas Hofer <niklas+dev@lanpartei.de>
2014-08-05 14:00:42 +0200

    hope that page takes the first window

--- features/support/page_load.rb
+++ features/support/page_load.rb
@@ -8,10 +8,8 @@ module PageLoadSupport
 
   def wait_for_the_page_to_be_loaded
     unless page.mode == :rack_test
-      within page.send(:scopes).first do
-        page.should have_css('html.loaded')
-        some_time_passes
-      end
+      page.should have_css('html.loaded')
+      some_time_passes
     end
   rescue Selenium::WebDriver::Error::UnhandledAlertError => e
     unless e.message =~ /An open modal dialog blocked the operation/
