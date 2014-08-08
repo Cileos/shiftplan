59b561469544edd5bde14202e98cff8c43d39068
Niklas Hofer <niklas+dev@lanpartei.de>
2014-08-08 16:02:18 +0200

    spec: create the records

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -4,4 +4,18 @@ class Setup < ActiveRecord::Base
   def execute!
     # TODO create ALL the stuff like Signup does currently
   end
+
+  class << self
+    def default_account_name
+      "Meine Firma"
+    end
+
+    def default_organization_name
+      "Meine Organisation"
+    end
+
+    def default_plan_name
+      "Mein erster Plan"
+    end
+  end
 end
