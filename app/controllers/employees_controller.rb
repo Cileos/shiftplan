48c94c7d55ecda781a416d71deb0ef1857f04d19
Niklas Hofer <niklas+dev@lanpartei.de>
2014-09-19 21:13:41 +0200

    decorate within view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -55,7 +55,7 @@ class EmployeesController < BaseController
         end
         @_current_org_set = true
       end
-    end.map(&:decorate)
+    end
   end
 
   # we want to use Employee#current_membership in views
