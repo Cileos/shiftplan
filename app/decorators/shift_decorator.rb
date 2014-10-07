f4e43f986bd6989f0a48f47eacfd45d3af078572
Niklas Hofer <niklas+dev@lanpartei.de>
2014-10-07 19:00:27 +0200

    no extrawurst for shifts needed anymore

--- app/decorators/shift_decorator.rb
+++ app/decorators/shift_decorator.rb
@@ -20,17 +20,4 @@ class ShiftDecorator < RecordDecorator
   def ends_on_focussed_day?
     focus_day == day + 1
   end
-
-  private
-
-    # Override method of TimePeriodFormatter module since in the case of shifts
-    # starts_at and ends_at are of class Time(not DateTime like for schedulings),
-    # so we have to manually add 1 day to the ends_at value for overnightables.
-    def length_in_minutes
-      @length_in_minutes ||= if is_overnight?
-        ((self_or_next_day.ends_at + 1.day) - self_or_prev_day.starts_at) / 60
-      else
-        (ends_at - starts_at) / 60
-      end
-    end
 end
