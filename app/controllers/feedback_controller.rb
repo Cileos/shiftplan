7ba113982b96a658f5747324ef7505a480f68b48
Niklas Hofer <niklas+dev@lanpartei.de>
2014-11-14 13:31:29 +0100

    offer non-js option for feedback
    Mainly, this avoids error messages caused by stupid crawlers following
    AJAX links willy-nilly.

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,14 @@
 class FeedbackController < BaseController
   skip_before_filter :authenticate_user!
 
-  respond_to :js
+  respond_to :js, :html
 
   def create
-    create! do
-      flash[:notice] = t(:'feedback.thank_you')
-      request.referer
+    create! do |success|
+      success.html do
+        flash[:notice] = t(:'feedback.thank_you')
+        redirect_to root_url
+      end
     end
   end
 end
