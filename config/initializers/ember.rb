1ad87a2a440948299989525bacf5ec57f90676a1
Niklas Hofer <niklas+dev@lanpartei.de>
2015-01-18 20:16:38 +0700

    `ember build` is really slow on niklas small machine

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,6 +1,6 @@
 EmberCLI.configure do |c|
   ember_root = ENV['CLOCKWORK_EMBER_ROOT']
   if !ember_root.nil? && !ember_root.empty?
-    c.app :frontend, path: ember_root
+    c.app :frontend, path: ember_root, build_timeout: 10
   end
 end
